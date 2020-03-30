4b40f8736c822d38226d45db85794e8ef323056d
khmuminov <khmuminov@gmail.com>
2020-03-30 18:23:10 +0500

    [Added] OC-229 #comment Implementing Add group test on localhost. #time 1h

--- test/src/main/java/TestStandalone.java
+++ test/src/main/java/TestStandalone.java
@@ -1,101 +1,39 @@
-import org.openqa.selenium.By;
-import org.openqa.selenium.Platform;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.chrome.ChromeOptions;
-import org.openqa.selenium.firefox.FirefoxDriver;
-import org.openqa.selenium.firefox.FirefoxDriverLogLevel;
-import org.openqa.selenium.firefox.FirefoxOptions;
-import org.openqa.selenium.logging.LogEntries;
-import org.openqa.selenium.logging.LogLevelMapping;
-import org.openqa.selenium.logging.LogType;
-import org.openqa.selenium.logging.LoggingPreferences;
-import org.openqa.selenium.remote.BrowserType;
-import org.openqa.selenium.remote.CapabilityType;
-import org.openqa.selenium.remote.DesiredCapabilities;
-
 import java.net.MalformedURLException;
-import java.util.concurrent.TimeUnit;
-import java.util.logging.Level;
 
 public class TestStandalone {
     public static void main(String[] args) throws MalformedURLException {
 
-        /*System.setProperty("webdriver.gecko.driver","/home/khmuminov/geckodriver");
+        UserTest();
 
-        FirefoxOptions option = new FirefoxOptions();
-        DesiredCapabilities cap = DesiredCapabilities.firefox();
-        cap.setPlatform(Platform.LINUX);ll
-        cap.setBrowserName(BrowserType.FIREFOX);
+        GroupTest();
 
-        cap.setCapability(FirefoxOptions.FIREFOX_OPTIONS, option);
-
-        LoggingPreferences logPrefs = new LoggingPreferences();
-        logPrefs.enable(LogType.PERFORMANCE, Level.ALL);
-
-        cap.setCapability(CapabilityType.LOGGING_PREFS, logPrefs);
-
-        FirefoxDriver driver=new FirefoxDriver(cap);
-        driver.get("http://oc-demo.westeurope.cloudapp.azure.com:8888/login");
-        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-        WebElement element_login=driver.findElementById("login_email");
-        element_login.sendKeys("khmuminov@gmail.com");
-        WebElement element_password=driver.findElementById("login_password");
-        element_password.sendKeys("12345678");
-        WebElement buttonConnect=driver.findElement(By.xpath("//button"));
-
-        buttonConnect.click();*/
+    }
 
 
+    private static void GroupTest() throws MalformedURLException {
 
-        //System.out.print(driver.manage().logs().get(LogType.PROFILER));
+        TestGroup testGroup = new TestGroup();
+        testGroup.setUp();
 
+        testGroup.afterTest();
 
+    }
 
+    private static void UserTest() throws MalformedURLException {
+        TestUser testUser = new TestUser();
 
+        testUser.setUp();
 
-      /* System.setProperty("webdriver.chrome.driver","/home/khmuminov/chromedriver");
-        ChromeOptions options = new ChromeOptions();
-        // add whatever extensions you need
-        // for example I needed one of adding proxy, and one for blocking
-        // images
-        // options.addExtensions(new File&#40;file, "proxy.zip"&#41;);
-        // options.addExtensions(new File&#40;"extensions",
-        // "Block-image_v1.1.crx"&#41;);
+        testUser.SimpleTest();
 
-        DesiredCapabilities cap = new DesiredCapabilities();
-        cap.setBrowserName("chrome");
-        cap.setCapability(ChromeOptions.CAPABILITY, options);
+        testUser.LoginTest();
 
-        // set performance logger
-        // this sends Network.enable to chromedriver
-        LoggingPreferences logPrefs = new LoggingPreferences();
-        logPrefs.enable(LogType.PERFORMANCE, Level.ALL);
-        cap.setCapability(CapabilityType.LOGGING_PREFS, logPrefs);
+        testUser.AddUserTest();
 
-        ChromeDriver driver = new ChromeDriver();
-        driver.get("http://oc-demo.westeurope.cloudapp.azure.com:8888/login");
-        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        testUser.UpdateUserTest();
 
-        //LogEntries logs = driver.manage().logs().get(LogType.PERFORMANCE);
+        testUser.DeleteUserTest();
 
-        //System.out.println(logs);*/
-
-
-      TestWithServer test = new TestWithServer();
-      test.setUp();
-
-      test.SimpleTest();
-
-      test.LoginTest();
-
-      test.AddUserTest();
-
-      test.UpdateUserTest();
-
-      test.DeleteUserTest();
-
-
-
+        testUser.afterTest();
     }
 }
