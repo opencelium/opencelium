0d1ed6904444e2b0b97a3cb49590aa406a862cbd
ruzrulit <ruzalin.galiev@becon.de>
2020-04-07 14:23:06 +0200

    [Modfied] OC-160 #comment preparation to release v1.1 #time 14h

--- src/frontend/cross-tab-auth.js
+++ src/frontend/cross-tab-auth.js
@@ -1,6 +1,5 @@
-
 /*
- * Copyright (C) <2019>  <becon GmbH>
+ * Copyright (C) <2020>  <becon GmbH>
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
