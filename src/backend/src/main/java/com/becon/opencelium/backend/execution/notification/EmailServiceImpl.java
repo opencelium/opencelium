bb49955d600913fb3c9259639c3eea64d1b8221b
bobmustaev <bobur.mustaev@becon.de>
2020-07-06 15:23:33 +0200

    [Fixes] OC-57 small fixes #time 10m

--- src/backend/src/main/java/com/becon/opencelium/backend/execution/notification/EmailServiceImpl.java
+++ src/backend/src/main/java/com/becon/opencelium/backend/execution/notification/EmailServiceImpl.java
@@ -1,6 +1,7 @@
 package com.becon.opencelium.backend.execution.notification;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.mail.MailException;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
@@ -12,13 +13,22 @@ public class EmailServiceImpl implements EmailService {
     @Autowired
     private JavaMailSender emailSender;
 
+    @Autowired
+    private Environment env;
+
     @Override
     public void sendMessage(String to, String subject, String text) {
+
+        String senderName = env.getProperty("opencelium.mail.from");
+
         try {
             SimpleMailMessage message = new SimpleMailMessage();
             message.setTo(to);
             message.setSubject(subject);
             message.setText(text);
+            if (senderName != null && !senderName.isEmpty()) {
+                message.setFrom(senderName);
+            }
 
             emailSender.send(message);
         } catch (MailException exception) {
