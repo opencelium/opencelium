30ef44914d5961c10ea5f74838729e6f72a21746
ruzrulit <ruzalin.galiev@becon.de>
2021-01-06 18:27:40 +0100

    [Added] OC-286 #comment created update assistant redux blocks #time 3h

---
+++ src/frontend/app/reducers/update_assistant.js
@@ -0,0 +1,46 @@
+/*
+ * Copyright (C) <2020>  <becon GmbH>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, version 3 of the License.
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+import {fromJS} from 'immutable';
+
+import {UpdateAssistantAction} from '@utils/actions';
+import {API_REQUEST_STATE} from "@utils/constants/app";
+
+
+const initialState = fromJS({
+    fetchingUpdateAppVersion: API_REQUEST_STATE.INITIAL,
+    updateAppVersion: '',
+    error: null,
+    message: {},
+});
+
+/**
+ * redux reducer for update assistant
+ */
+const reducer = (state = initialState, action) => {
+    switch (action.type) {
+        case UpdateAssistantAction.FETCH_UPDATEAPPVERSION:
+            return state.set('fetchingUpdateAppVersion', API_REQUEST_STATE.START).set('error', null);
+        case UpdateAssistantAction.FETCH_UPDATEAPPVERSION_FULFILLED:
+            return state.set('fetchingUpdateAppVersion', API_REQUEST_STATE.FINISH).set('updateAppVersion', action.payload.version);
+        case UpdateAssistantAction.FETCH_UPDATEAPPVERSION_REJECTED:
+            return state.set('fetchingUpdateAppVersion', API_REQUEST_STATE.ERROR).set('error', action.payload);
+        default:
+            return state;
+    }
+};
+
+
+export {initialState, reducer as update_assistant};
