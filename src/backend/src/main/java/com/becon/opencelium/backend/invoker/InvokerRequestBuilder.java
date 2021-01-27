8ccdd452ad76e10c6d5a9d48e069b3c5b69b784e
Bob Mustaev <bobur.mustaev@gmail.com>
2021-01-27 13:57:53 +0100

    [Modified] OC-283 modified function that ignores ssl handshake #time 10m

--- src/backend/src/main/java/com/becon/opencelium/backend/invoker/InvokerRequestBuilder.java
+++ src/backend/src/main/java/com/becon/opencelium/backend/invoker/InvokerRequestBuilder.java
@@ -119,16 +119,16 @@ public class InvokerRequestBuilder{
     private RestTemplate getRestTemplate() {
 
         try {
-            TrustStrategy acceptingTrustStrategy = new TrustStrategy() {
-                @Override
-                public boolean isTrusted(X509Certificate[] x509Certificates, String s) throws CertificateException {
-                    return true;
-                }
-            };
-            SSLContext sslContext = org.apache.http.ssl.SSLContexts.custom().loadTrustMaterial(null, acceptingTrustStrategy).build();
-            SSLConnectionSocketFactory csf = new SSLConnectionSocketFactory(sslContext, new NoopHostnameVerifier());
-            CloseableHttpClient httpClient = HttpClients.custom().setSSLSocketFactory(csf).build();
-            HttpComponentsClientHttpRequestFactory requestFactory = new HttpComponentsClientHttpRequestFactory();
+            TrustStrategy acceptingTrustStrategy = (X509Certificate[] chain, String authType) -> true;
+            SSLContext sslContext = org.apache.http.ssl.SSLContexts.custom()
+                    .loadTrustMaterial(null, acceptingTrustStrategy)
+                    .build();
+            SSLConnectionSocketFactory csf = new SSLConnectionSocketFactory(sslContext);
+            CloseableHttpClient httpClient = HttpClients.custom()
+                    .setSSLSocketFactory(csf)
+                    .build();
+            HttpComponentsClientHttpRequestFactory requestFactory =
+                    new HttpComponentsClientHttpRequestFactory();
             requestFactory.setHttpClient(httpClient);
             RestTemplate restTemplate = new RestTemplate(requestFactory);
             return restTemplate;
