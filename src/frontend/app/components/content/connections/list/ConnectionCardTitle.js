13558fb488069b5077b923373f89335665f92f23
ruzrulit <ruzalin.galiev@becon.de>
2021-02-16 09:53:16 +0100

    [Modified] OC-317 #comment redesigned ConnectionCardTitle component #time 2h

--- src/frontend/app/components/content/connections/list/ConnectionCardTitle.js
+++ src/frontend/app/components/content/connections/list/ConnectionCardTitle.js
@@ -3,25 +3,112 @@ import FontIcon from "@basic_components/FontIcon";
 import { Container, Row, Col } from "react-grid-system";
 
 import styles from '@themes/default/content/connections/list.scss'
+import {checkImage} from "@utils/app";
 
-export default (props) => {
-    const {fromInvoker, toInvoker} = props;
+//add canvas when icon will be ready
+const DefaultIcon = (props) => {
     return (
-        <Container className={styles.connection_card_title}>
-            <Row className={styles.icons}>
-                <Col md={5} className={styles.from}>
-                    <img alt={fromInvoker.name} src={fromInvoker.icon}/>
-                </Col>
-                <Col md={2} className={styles.arrow}>
-                    <FontIcon value={'arrow_forward'} className={styles.arrow_icon}/>
-                </Col>
-                <Col md={5} className={styles.to}>
-                    <img alt={toInvoker.name} src={toInvoker.icon}/>
-                </Col>
-            </Row>
-            <Row>
-                <Col className={styles.title} md={12}>{props.title}</Col>
-            </Row>
-        </Container>
+        <span>{props.name}</span>
     )
+}
+
+export default class ConnectionCardTitle extends React.Component{
+    constructor(props) {
+        super(props);
+        this.state = {
+            isMouseOver: false,
+            isCorrectFromIcon: false,
+            isCorrectToIcon: false,
+        };
+        this.notMounted = true;
+    }
+
+    componentDidMount(){
+        const {fromInvoker, toInvoker} = this.props;
+        checkImage(fromInvoker.icon, () => this.setState({isCorrectFromIcon: true}), () => this.setState({isCorrectFromIcon: false}));
+        checkImage(toInvoker.icon, () => this.setState({isCorrectToIcon: true}), () => this.setState({isCorrectToIcon: false}));
+        this.notMounted = false;
+    }
+
+    handleMouseOver(){
+        this.setState({
+            isMouseOver: true,
+        })
+    }
+
+    handleMouseLeave(){
+        this.setState({
+            isMouseOver: false,
+        })
+    }
+
+    render() {
+        if(this.notMounted){
+            return null;
+        }
+        const {isMouseOver, isCorrectFromIcon, isCorrectToIcon} = this.state;
+        const {fromInvoker, toInvoker, title} = this.props;
+        let iconsStyles = {
+            opacity: 0.5,
+        };
+        let fromStyles = {
+            minHeight: '20px !important',
+            lineHeight: '20px',
+        };
+        let toStyles = {
+            minHeight: '20px !important',
+            lineHeight: '20px',
+        };
+        let arrowIconStyles = {
+            marginTop: 0,
+        };
+        let imgStyles = {
+            maxHeight: '20px',
+        };
+        if(isMouseOver){
+            iconsStyles = {
+                opacity: 1,
+            }
+            fromStyles = {
+                minHeight: '40px !important',
+                lineHeight: '40px',
+            };
+            toStyles = {
+                minHeight: '40px !important',
+                lineHeight: '40px',
+            };
+            arrowIconStyles = {
+                marginTop: '8px',
+            };
+            imgStyles = {
+                maxHeight: '40px',
+            };
+        }
+        return (
+            <Container className={styles.connection_card_title} onMouseOver={::this.handleMouseOver} onMouseLeave={::this.handleMouseLeave}>
+                <Row className={styles.icons} style={iconsStyles}>
+                    <Col md={5} className={styles.from} style={fromStyles}>
+                        {isCorrectFromIcon ?
+                            <img alt={fromInvoker.name} src={fromInvoker.icon} style={imgStyles}/>
+                        :
+                            <DefaultIcon name={fromInvoker.name} isMouseOver={isMouseOver}/>
+                        }
+                    </Col>
+                    <Col md={2} className={styles.arrow}>
+                        <FontIcon value={'arrow_forward'} className={styles.arrow_icon} style={arrowIconStyles}/>
+                    </Col>
+                    <Col md={5} className={styles.to} style={toStyles}>
+                        {isCorrectToIcon ?
+                            <img alt={toInvoker.name} src={toInvoker.icon} style={imgStyles}/>
+                            :
+                            <DefaultIcon name={toInvoker.name} isMouseOver={isMouseOver}/>
+                        }
+                    </Col>
+                </Row>
+                <Row>
+                    <Col className={styles.title} md={12}>{title}</Col>
+                </Row>
+            </Container>
+        )
+    }
 }
