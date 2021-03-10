d405df0604cc5e0844433004e194de065caefeba
ruzrulit <ruzalin.galiev@becon.de>
2021-03-10 12:36:14 +0500

    [Modified] OC-321 #comment added current jobs panel #time 2h

--- src/frontend/app/components/content/dashboard/view/settings.js
+++ src/frontend/app/components/content/dashboard/view/settings.js
@@ -7,11 +7,11 @@ export const HAS_DASHBOARD_WIDGET_ENGINE = true;
 
 export const INITIAL_LAYOUT = [
     {icon: 'cable', tooltip: 'Connection Overview', i: 'CONNECTION_OVERVIEW', x: 0, y: 0, w: 6, h: 4, minW: 6, minH: 4},
+    {icon: 'date_range', tooltip: 'Current Scheduler', i: 'CURRENT_SCHEDULER', x: 10, y: 0, w: 6, h: 3, minW: 6, minH: 3},
 ];
 
 export const INITIAL_TOOLBOX = [
     {icon: 'analytics', tooltip: 'Monitoring Boards', i: 'MONITORING_BOARDS', x: 0, y: 0, w: 6, h: 4, minW: 6, minH: 4},
-    {icon: 'date_range', tooltip: 'Current Scheduler', i: 'CURRENT_SCHEDULER', x: 10, y: 0, w: 6, h: 3, minW: 6, minH: 3},
 ];
 
 export const WIDGET_LIST = {
