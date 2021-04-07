df6deb0d5703171fffd17834970721a935d72eed
ruzrulit <ruzalin.galiev@becon.de>
2021-04-07 15:49:52 +0500

    [Documented] OC-160 #comment modified copyright #time 1h

--- src/frontend/app/classes/components/general/basic_components/CBodyEditor.js
+++ src/frontend/app/classes/components/general/basic_components/CBodyEditor.js
@@ -1,3 +1,18 @@
+/*
+ * Copyright (C) <2021>  <becon GmbH>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, version 3 of the License.
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
 import {convertFieldNameForBackend} from "@change_component/form_elements/form_connection/form_methods/help";
 import CBindingItem from "@classes/components/content/connection/field_binding/CBindingItem";
 import {CONNECTOR_TO} from "@classes/components/content/connection/CConnectorItem";
