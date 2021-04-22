f548cf010c869340781f7c0eeec54bf44532ca66
ruzrulit <ruzalin.galiev@becon.de>
2021-04-22 13:54:56 +0500

    [Modified] OC-289 #comment changed system overview details #time 3h

--- src/frontend/app/components/content/update_assistant/FinishUpdate.js
+++ src/frontend/app/components/content/update_assistant/FinishUpdate.js
@@ -17,7 +17,6 @@ import React from 'react';
 import {withTranslation} from "react-i18next";
 import styles from "@themes/default/content/update_assistant/main";
 import Translate from "@components/general/app/Translate";
-import {history} from "@components/App";
 
 
 @withTranslation('update_assistant')
@@ -29,15 +28,15 @@ class FinishUpdate extends React.Component{
     render(){
         const {t} = this.props;
         const updateLogLink = 'https://docs.opencelium.io/en/prod/gettinginvolved/administration.html';
-        const logsLinkText = t('FORM.FINISH.LOGS_LINK_TEXT');
+        const restoreLinkText = t('FORM.FINISH.RESTORE_LINK_TEXT');
         return(
             <div className={styles.finish_update}>
                 <div className={styles.header}>{t('FORM.FINISH.HEADER')}</div>
                 <div>{t('FORM.FINISH.NEW_VERSION')}</div>
                 <Translate i18nKey="update_assistant:FORM.FINISH.LOG_MESSAGE"
-                           values={{logsLinkText}}
+                           values={{restoreLinkText}}
                            components={[
-                               <a href={updateLogLink} target={'_blank'} children={logsLinkText}/>
+                               <a href={updateLogLink} target={'_blank'} children={restoreLinkText}/>
                            ]}/>
                 <div><span className={styles.hint}>{t('FORM.FINISH.HINT')}</span>: {t('FORM.FINISH.CLEAR_CACHE')}</div>
             </div>
