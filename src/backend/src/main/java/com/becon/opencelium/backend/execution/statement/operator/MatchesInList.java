539630dee24fbd0f03b39af387b1009efa7c489f
Bob Mustaev <bobur.mustaev@gmail.com>
2021-05-06 12:12:51 +0500

    [Fixed] OC-337 removed table #time 30m

--- src/backend/src/main/java/com/becon/opencelium/backend/execution/statement/operator/MatchesInList.java
+++ src/backend/src/main/java/com/becon/opencelium/backend/execution/statement/operator/MatchesInList.java
@@ -7,10 +7,12 @@ public class MatchesInList implements Operator {
     @Override
     public <T, S> boolean compare(T val1, S val2) {
         ArrayList values = (ArrayList) val2;
-
+        Like like = new Like();
         for (Object v : values) {
-
+            if (like.compare(val1, v)) {
+                return true;
+            }
         }
-        return values.contains(val1);
+        return false;
     }
 }
