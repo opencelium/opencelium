1a26d2681c2fc1e38fbc68e2a031874fbad240a8
Bob Mustaev <bobur.mustaev@gmail.com>
2021-05-11 12:51:34 +0500

    [Fixed] OC-337 fixed bug when executing AllowList operator #time 50m

--- src/backend/src/main/java/com/becon/opencelium/backend/execution/statement/operator/MatchesInList.java
+++ src/backend/src/main/java/com/becon/opencelium/backend/execution/statement/operator/MatchesInList.java
@@ -1,12 +1,13 @@
 package com.becon.opencelium.backend.execution.statement.operator;
 
-import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class MatchesInList implements Operator {
 
     @Override
     public <T, S> boolean compare(T val1, S val2) {
-        ArrayList values = (ArrayList) val2;
+        String[] values = (String[])val2;
         Like like = new Like();
         for (Object v : values) {
             if (like.compare(val1, v)) {
