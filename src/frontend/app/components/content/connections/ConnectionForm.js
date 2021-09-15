ef641862b0627c5161109d1ae9cd59c5e28dca5d
ruzrulit <ruzalin.galiev@becon.de>
2021-09-15 15:08:47 +0200

    [Fixed] OC-160 #comment fixed bug in JsonViewer #time 2h

--- src/frontend/app/components/content/connections/ConnectionForm.js
+++ src/frontend/app/components/content/connections/ConnectionForm.js
@@ -40,7 +40,7 @@ export function ConnectionForm(type) {
                     },
                     connection: CConnection.createConnection(null),
                     entity: null,
-                    isNewConnectionView: true,
+                    isNewConnectionView: false,
                 };
             }
 
