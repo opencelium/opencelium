87e1d47c07bd9345bb51354277c8490640eee019
ruzrulit <ruzalin.galiev@becon.de>
2021-09-21 10:52:38 +0200

    [Modified] OC-160 #comment minor changes in invoker entity #time 1h

--- src/frontend/app/components/content/invokers/update/InvokerUpdate.js
+++ src/frontend/app/components/content/invokers/update/InvokerUpdate.js
@@ -14,40 +14,19 @@
  */
 
 import React, {Component} from 'react';
-import {connect} from 'react-redux';
-import {withTranslation} from 'react-i18next';
-import {InvokerPermissions} from "@utils/constants/permissions";
-import {permission} from "@decorators/permission";
-import {SingleComponent} from "@decorators/SingleComponent";
-import {InvokerForm} from "@components/content/invokers/InvokerForm";
-import {updateInvoker} from "@actions/invokers/update";
-import {checkName, fetchInvoker} from "@actions/invokers/fetch";
-
-
-function mapStateToProps(state){
-    const auth = state.get('auth');
-    const invokers = state.get('invokers');
-    return{
-        authUser: auth.get('authUser'),
-        invoker: invokers.get('invoker'),
-        fetchingInvoker: invokers.get('fetchingInvoker'),
-        updatingInvoker: invokers.get('updatingInvoker'),
-        error: invokers.get('error'),
-    };
-}
-
-function mapConnector(invoker){
-    return {xml: invoker.getXml()};
-}
 
 /**
- * Component to Add Invoker
+ * Component to Update Invoker
  */
-@connect(mapStateToProps, {updateInvoker, fetchInvoker, checkName})
-@permission(InvokerPermissions.UPDATE, true)
-@withTranslation(['invokers', 'app'])
-@SingleComponent('invoker', 'updating', [], mapConnector)
-@InvokerForm('update')
-class InvokerUpdate extends Component{}
+class InvokerUpdate extends Component{
+
+    constructor(props){
+        super(props);
+    }
+
+    render(){
+        return <div style={{fontSize: '30px', textAlign: 'center', marginTop: '150px'}}>Sorry, you do not have a subscription to use this function</div>;
+    }
+}
 
 export default InvokerUpdate;
