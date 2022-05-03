9522ea7933e69f2ae1866fdbcb2f85edf179eff3
Ruzalin Galiev <ruzalin.galiev@becon.de>
2022-05-03 18:13:55 +0200

    [Fixed] OC-457 #comment fixed webpack build settings #time 10m

--- src/frontend/webpack.build.js
+++ src/frontend/webpack.build.js
@@ -1,3 +1,5 @@
+const integrateLibs = require('./plugins/LibsIntegration');
+integrateLibs();
 
 const {getConfig} = require('./webpack.config');
 const config = getConfig({envVar: {'process.env.isProduction': true}});
