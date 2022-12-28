a0ac50085fb327200880c82e891c6d4e759a17c5
bobmustaev <bobur.mustaev@becon.de>
2022-12-28 10:52:11 +0500

    [Added] OC-480 #comment fized bug where uri was empty when responsing exception #time 1h

--- src/backend/src/main/java/com/becon/opencelium/backend/exception/handler/ResponseExceptionHandler.java
+++ src/backend/src/main/java/com/becon/opencelium/backend/exception/handler/ResponseExceptionHandler.java
@@ -1,13 +1,13 @@
 package com.becon.opencelium.backend.exception.handler;
 
 import com.becon.opencelium.backend.resource.error.ErrorResource;
-import org.springframework.http.HttpHeaders;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.context.request.ServletWebRequest;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
@@ -16,11 +16,17 @@ import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExcep
 @ControllerAdvice
 public class ResponseExceptionHandler extends ResponseEntityExceptionHandler {
 
+    @Value("${opencelium.debug_mode}")
+    private boolean debugMode;
+
     @ExceptionHandler(value = { Exception.class })
     @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
-    public ResponseEntity<Object> unknownException(Exception ex, WebRequest req) {
-        ex.printStackTrace();
-        ErrorResource errorResource = new ErrorResource(ex, HttpStatus.INTERNAL_SERVER_ERROR, req.getContextPath());
+    public ResponseEntity<Object> allExceptionHandler(Exception ex, WebRequest req) {
+        if (debugMode) {
+            ex.printStackTrace();
+        }
+        String uri = ((ServletWebRequest)req).getRequest().getRequestURI();
+        ErrorResource errorResource = new ErrorResource(ex, HttpStatus.INTERNAL_SERVER_ERROR, uri);
         return ResponseEntity.internalServerError().body(errorResource);
     }
 }
