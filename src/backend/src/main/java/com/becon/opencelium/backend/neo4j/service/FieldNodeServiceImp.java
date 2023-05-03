48cfb513d46cec1dd6a1e1ef3f2efbaea954f63a
bobmustaev <bobur.mustaev@becon.de>
2023-05-03 11:24:15 +0200

    [Fixed] OC-599 #comment fixed bug where an array could ben ull #time 10m

--- src/backend/src/main/java/com/becon/opencelium/backend/neo4j/service/FieldNodeServiceImp.java
+++ src/backend/src/main/java/com/becon/opencelium/backend/neo4j/service/FieldNodeServiceImp.java
@@ -101,7 +101,8 @@ public class FieldNodeServiceImp implements FieldNodeService {
             }
             format = methodNode.getResponseNode().getSuccess().getBody().getFormat();
             firstField = StringUtility.removeSquareBraces(firstField);
-            currentField = fieldNodeRepository.findFirstFieldInResponse(connectionId,color,result,firstField).get(0);
+            currentField = fieldNodeRepository.findFirstFieldInResponse(connectionId,color,result,firstField)
+                    .stream().findFirst().orElse(null);
         } else {
             if (path.isEmpty()){
                 firstField = field;
@@ -110,7 +111,8 @@ public class FieldNodeServiceImp implements FieldNodeService {
             }
             format = methodNode.getRequestNode().getBodyNode().getFormat();
             firstField = StringUtility.removeSquareBraces(firstField);
-            currentField = fieldNodeRepository.findFirstFieldInRequest(connectionId,color,firstField).get(0);
+            currentField = fieldNodeRepository.findFirstFieldInRequest(connectionId,color,firstField)
+                    .stream().findFirst().orElse(null);
         }
 
         if (currentField == null){
