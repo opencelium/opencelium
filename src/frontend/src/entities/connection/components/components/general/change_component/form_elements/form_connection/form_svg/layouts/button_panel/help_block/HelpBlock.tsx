f80eae53ecac6ef637664427f794746d0ff12a52
Ruzalin Galiev <ruzalin.galiev@becon.de>
2023-05-17 16:54:48 +0500

    [Modified] OC-584 #comment example of animation #time 1h

--- src/frontend/src/entities/connection/components/components/general/change_component/form_elements/form_connection/form_svg/layouts/button_panel/help_block/HelpBlock.tsx
+++ src/frontend/src/entities/connection/components/components/general/change_component/form_elements/form_connection/form_svg/layouts/button_panel/help_block/HelpBlock.tsx
@@ -36,32 +36,87 @@ import {setModalConnectionData} from "@root/redux_toolkit/slices/ModalConnection
 import {CONNECTOR_FROM} from "@classes/content/connection/CConnectorItem";
 import { Connector } from "@entity/connector/classes/Connector";
 import {API_REQUEST_STATE} from "@application/interfaces/IApplication";
+import {sortByIndex} from "@application/utils/utils";
+import {setVideoAnimationName} from "@root/redux_toolkit/slices/ConnectionSlice";
+
 
 const HelpBlock = () => {
-  const dispatch = useAppDispatch();
-  const {connectors, gettingConnectors} = Connector.getReduxState();
-  const [connection, setConnection] = useState(CConnection.createConnection(HelpBlockData));
-  const [isVisible, setIsVisible] = useState(false);
+    const dispatch = useAppDispatch();
+    const [index, setIndex] = useState<number>(-1);
+    const {connectors, gettingConnectors} = Connector.getReduxState();
+    const [connection, setConnection] = useState(CConnection.createConnection(HelpBlockData));
+    const [isVisible, setIsVisible] = useState(false);
+    const [allItems, setAllItems] = useState({fromConnector: [], toConnector: []});
 
-  useEffect(() => {
+    const { isButtonPanelOpened, videoAnimationName } = useAppSelector(
+        (state) => state.connectionReducer
+    );
+
+    useEffect(() => {
+        if(!isButtonPanelOpened){
+            setIndex(-1);
+            dispatch(setVideoAnimationName(''));
+        }
+    }), [isButtonPanelOpened];
+    useEffect(() => {
+        if(isButtonPanelOpened && videoAnimationName) {
+            if (index >= 0 && index < allItems.fromConnector.length + allItems.toConnector.length) {
+                if (index < allItems.fromConnector.length) {
+                    if (allItems.fromConnector[index].hasOwnProperty('type')) {
+                        connection.fromConnector.operators.push(allItems.fromConnector[index]);
+                    } else {
+                        connection.fromConnector.methods.push(allItems.fromConnector[index]);
+                    }
+                } else {
+                    if (allItems.toConnector[index - allItems.fromConnector.length].hasOwnProperty('type')) {
+                        connection.toConnector.operators.push(allItems.toConnector[index - allItems.fromConnector.length]);
+                    } else {
+                        connection.toConnector.methods.push(allItems.toConnector[index - allItems.fromConnector.length]);
+                    }
+                }
+                updateEntity(connection);
+                setTimeout(() => setIndex(index + 1), 1000);
+            }
+        }
+    }, [index, isButtonPanelOpened, videoAnimationName])
+
+    useEffect(() => {
+        if(allItems.fromConnector.length === 0 && allItems.toConnector.length === 0){
+            setAllItems({
+                fromConnector: sortByIndex([...connection.fromConnector.methods, ...connection.fromConnector.operators]),
+                toConnector: sortByIndex([...connection.toConnector.methods, ...connection.toConnector.operators])
+            });
+        }
+    }, [])
+    useEffect(() => {
       if(gettingConnectors === API_REQUEST_STATE.FINISH) {
           let fromConnector = connectors.find(c => c.connectorId === HelpBlockData.fromConnector.connectorId);
           let toConnector = connectors.find(c => c.connectorId === HelpBlockData.toConnector.connectorId);
           if (fromConnector && toConnector) {
               connection.fromConnector.id = fromConnector.connectorId;
               connection.fromConnector.title = fromConnector.title;
+              connection.fromConnector.methods = [];
+              connection.fromConnector.operators = [];
               //@ts-ignore
               connection.fromConnector.invoker = fromConnector.invoker;
               connection.fromConnector.setConnectorType(CONNECTOR_FROM);
               connection.toConnector.id = toConnector.connectorId;
               connection.toConnector.title = toConnector.title;
+              connection.toConnector.methods = [];
+              connection.toConnector.operators = [];
               //@ts-ignore
               connection.toConnector.invoker = toConnector.invoker;
               connection.toConnector.setConnectorType(CONNECTOR_FROM);
               setConnection(connection);
           }
       }
-  }, [gettingConnectors]);
+    }, [gettingConnectors]);
+
+    useEffect(() => {
+        if(index === -1){
+            setIndex(0)
+        }
+    }, [connection])
 
   function toggleVisible() {
     setIsVisible(!isVisible);
@@ -73,9 +128,6 @@ const HelpBlock = () => {
       dispatch(setModalConnectionData({connection}));
   }
 
-  const { isButtonPanelOpened, videoAnimationName } = useAppSelector(
-    (state) => state.connectionReducer
-  );
 
   return (
     <HelpBlockStyled isButtonPanelOpened={isButtonPanelOpened}>
