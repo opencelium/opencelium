c6624b0e3fe1167e3f9ea3085e1faae2282e546e
Ruzalin Galiev <ruzalin.galiev@becon.de>
2023-08-17 19:54:26 +0500

    [Modified] OC-699 #comment extract url file from compiled files during build #time 4h

---
+++ src/frontend/src/scripts/config.js
@@ -0,0 +1,17 @@
+window.config = {
+    env: {
+        urlInfo: {
+            port: {
+                api: 9090,
+                socket: 8082,
+                kibana: 5601,
+                neo4j: 7474
+            },
+            dev: {
+                port: 8888
+            },
+            protocol: "",
+            hostname: "",
+        }
+    }
+};
