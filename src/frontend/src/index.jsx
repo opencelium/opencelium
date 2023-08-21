21a52127a9fd7c2e0f7e7b22f7245b33eb238748
Ruzalin Galiev <ruzalin.galiev@becon.de>
2023-08-21 13:37:03 +0500

    [Modified] OC-699 #comment fixed relative path issue #time 30m

--- src/frontend/src/index.jsx
+++ src/frontend/src/index.jsx
@@ -12,7 +12,7 @@
  *  You should have received a copy of the GNU General Public License
  *  along with this program. If not, see <http://www.gnu.org/licenses/>.
  */
-
+import './scripts/config';
 import ReactDOM from 'react-dom';
 import React from 'react';
 import {Provider} from "react-redux";
