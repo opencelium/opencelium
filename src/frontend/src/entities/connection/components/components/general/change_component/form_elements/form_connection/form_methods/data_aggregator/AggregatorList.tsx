81a064ad6d216e75f92692a932e56f04057c77b6
Ruzalin Galiev <ruzalin.galiev@becon.de>
2023-08-23 00:05:35 +0500

    [Added] OC-707 #comment created aggregator list component file #time 10m

---
+++ src/frontend/src/entities/connection/components/components/general/change_component/form_elements/form_connection/form_methods/data_aggregator/AggregatorList.tsx
@@ -0,0 +1,14 @@
+import React, {FC, useState} from 'react';
+import {
+    AggregatorListProps,
+} from "./interfaces";
+
+
+const AggregatorList:FC<AggregatorListProps> = ({}) => {
+    return (
+        <React.Fragment>
+        </React.Fragment>
+    )
+}
+
+export default AggregatorList;
