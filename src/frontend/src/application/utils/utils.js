46f1660a4df8f2ea24d63aada36fa2b6bfe21f61
Ruzalin Galiev <ruzalin.galiev@becon.de>
2023-09-07 13:31:19 +0500

    [Modified] OC-718 #comment implemented highlighting #time 1h

--- src/frontend/src/application/utils/utils.js
+++ src/frontend/src/application/utils/utils.js
@@ -899,3 +899,23 @@ export const sortAlphabeticallyByKey = (array, key) => {
         return ((x < y) ? -1 : ((x > y) ? 1 : 0));
     });
 }
+
+export const getMarker = (editor, value, searchString) => {
+    const matches = [...value?.matchAll(`\\${searchString}`)];
+    let result = [];
+    for(let i = 0; i < matches.length; i++){
+        if (matches[i]?.index) {
+            const start = editor.session.doc.indexToPosition(matches[i].index);
+            const end = editor.session.doc.indexToPosition(matches[i].index + searchString.length);
+            result.push({
+                startRow: start.row,
+                startCol: start.column,
+                endRow: end.row,
+                endCol: end.column,
+                className: "error-ace-marker",
+                type: "text",
+            });
+        }
+    }
+    return result;
+}
