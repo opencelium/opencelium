e7b8aff559f4c1dacac63988263d64cdf2999ad6
NarzullaevObidjon <obidsattarovich3600@gmail.com>
2023-12-13 17:09:45 +0500

    [Modified] OC-773 #comment Added requiredData field to ConnectorEx. Modified collecting data for execution #time 1h

--- src/backend/src/main/java/com/becon/opencelium/backend/mapper/execution/ConnectorExMapper.java
+++ src/backend/src/main/java/com/becon/opencelium/backend/mapper/execution/ConnectorExMapper.java
@@ -2,6 +2,7 @@ package com.becon.opencelium.backend.mapper.execution;
 
 import com.becon.opencelium.backend.database.mongodb.entity.ConnectorMng;
 import com.becon.opencelium.backend.mapper.base.Mapper;
+import com.becon.opencelium.backend.mapper.utils.HelperMapper;
 import com.becon.opencelium.backend.resource.execution.ConnectorEx;
 import org.mapstruct.Mapping;
 import org.mapstruct.Named;
@@ -13,18 +14,20 @@ import org.mapstruct.ReportingPolicy;
         unmappedSourcePolicy = ReportingPolicy.IGNORE,
         uses = {
                 OperatorExMapper.class,
-                OperationMngMapper.class
+                OperationMngMapper.class,
+                HelperMapper.class
         }
 )
 @Named("connectorExMapper")
 public interface ConnectorExMapper extends Mapper<ConnectorEx, ConnectorMng> {
 
-    @Mapping(target = "methods", qualifiedByName = {"operationMngMapper","toEntityAll"})
-    @Mapping(target = "operators", qualifiedByName = {"operatorExMapper","toEntityAll"})
+    @Mapping(target = "methods", qualifiedByName = {"operationMngMapper", "toEntityAll"})
+    @Mapping(target = "operators", qualifiedByName = {"operatorExMapper", "toEntityAll"})
+    @Mapping(target = "requiredData", source = "connectorId", qualifiedByName = {"helperMapper", "getRequiredData"})
     @Named("toEntity")
     ConnectorEx toEntity(ConnectorMng dto);
 
-    default ConnectorMng toDTO(ConnectorEx entity){
+    default ConnectorMng toDTO(ConnectorEx entity) {
         return null;
     }
 }
--- src/backend/src/main/java/com/becon/opencelium/backend/resource/execution/ConnectorEx.java
+++ src/backend/src/main/java/com/becon/opencelium/backend/resource/execution/ConnectorEx.java
@@ -1,10 +1,12 @@
 package com.becon.opencelium.backend.resource.execution;
 
 import java.util.List;
+import java.util.Map;
 
 public class ConnectorEx {
     private List<OperationDTO> methods;
     private List<OperatorEx> operators;
+    private Map<String, String> requiredData;
 
     public ConnectorEx() {
     }
@@ -23,5 +25,13 @@ public class ConnectorEx {
 
     public void setOperators(List<OperatorEx> operators) {
         this.operators = operators;
+    }
+
+    public Map<String, String> getRequiredData() {
+        return requiredData;
+    }
+
+    public void setRequiredData(Map<String, String> requiredData) {
+        this.requiredData = requiredData;
     }
 }
