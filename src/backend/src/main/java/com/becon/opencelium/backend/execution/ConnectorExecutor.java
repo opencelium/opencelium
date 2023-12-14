81052bf4a995979fcd29854d53e98cacfff1ff88
Bekhzod Kurbonboev <bekhzod_kurbonboev@epam.com>
2023-12-14 23:44:26 +0500

    [Modified] OC-721 #comment Refactored ConnectorExecutor class to use new OperatorEx instead of OperatorDTO #time 1h

--- src/backend/src/main/java/com/becon/opencelium/backend/execution/ConnectorExecutor.java
+++ src/backend/src/main/java/com/becon/opencelium/backend/execution/ConnectorExecutor.java
@@ -2,34 +2,38 @@ package com.becon.opencelium.backend.execution;
 
 import com.becon.opencelium.backend.execution.builder.RequestEntityBuilder;
 import com.becon.opencelium.backend.execution.oc721.Operation;
+import com.becon.opencelium.backend.resource.execution.ConnectorEx;
 import com.becon.opencelium.backend.resource.execution.Executable;
 import com.becon.opencelium.backend.resource.execution.OperationDTO;
-import com.becon.opencelium.backend.resource.execution.OperatorDTO;
-import com.becon.opencelium.backend.resource.execution.OperatorType;
-import com.becon.opencelium.backend.resource.execution.SchemaDTO;
+import com.becon.opencelium.backend.resource.execution.OperatorEx;
 import org.springframework.http.RequestEntity;
 import org.springframework.http.ResponseEntity;
 import org.springframework.util.ObjectUtils;
 import org.springframework.web.client.RestTemplate;
 
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.LinkedHashMap;
 import java.util.List;
+import java.util.Objects;
 import java.util.PriorityQueue;
 
 public class ConnectorExecutor {
-
-    // stores OperationDTO, and OperatorDTO in sorted order by execOrder;
-    private final PriorityQueue<Executable> executables;
+    private final ConnectorEx connector;
     private final ExecutionManager executionManager;
     private RestTemplate restTemplate;
 
-    public ConnectorExecutor(PriorityQueue<Executable> executables, ExecutionManager executionManager) {
-        this.executables = executables;
+    public ConnectorExecutor(ConnectorEx connector, ExecutionManager executionManager) {
+        this.connector = connector;
         this.executionManager = executionManager;
     }
 
     private void start() {
+        // stores OperationDTO, and OperatorDTO in sorted order by execOrder;
+        PriorityQueue<Executable> executables = new PriorityQueue<>(Comparator.comparing(Executable::getExecOrder));
+        executables.addAll(connector.getMethods());
+        executables.addAll(connector.getOperators());
+
         List<Executable> body;
 
         while (!executables.isEmpty()) {
@@ -60,9 +64,9 @@ public class ConnectorExecutor {
         if (body.get(index) instanceof OperationDTO) {
             executeOperation(body, index);
         } else {
-            OperatorDTO current = (OperatorDTO) body.get(index);
+            OperatorEx current = (OperatorEx) body.get(index);
 
-            if (current.getType() == OperatorType.IF) {
+            if (Objects.equals(current.getType(), "if")) {
                 executeIfOperator(body, index);
             } else if (current.getIterator() != null) {
                 executeForOperator(body, index);
@@ -77,7 +81,8 @@ public class ConnectorExecutor {
 
         RequestEntity<?> requestEntity = RequestEntityBuilder.start()
                 .forOperation(operationDTO)
-                .usingReferences(executionManager::getValue)
+                // TODO implement Object -> SchemaDTO
+                .usingReferences(null)
                 .createRequest();
 
         ResponseEntity<String> responseEntity = this.restTemplate.exchange(requestEntity, String.class);
@@ -100,12 +105,13 @@ public class ConnectorExecutor {
     }
 
     private void executeIfOperator(List<Executable> body, int index) {
-        OperatorDTO operatorDTO = (OperatorDTO) body.get(index);
+        OperatorEx operatorDTO = (OperatorEx) body.get(index);
 
-        SchemaDTO leftValue = executionManager.getValue(operatorDTO.getLeftValueReference());
-        SchemaDTO rightValue = executionManager.getValue(operatorDTO.getRightValueReference());
+        Object leftValue = executionManager.getValue(operatorDTO.getCondition().getLeft());
+        Object rightValue = executionManager.getValue(operatorDTO.getCondition().getRight());
 
-        boolean result = operatorDTO.getLogicalOperator().algorithm.apply(leftValue, rightValue);
+        // TODO use Factory to get operator
+        boolean result = false;
 
         // when 'if' evaluates to false, then remove its body.
         // Body contains executables that has 'execOrder' starting with 'execOrder' of 'if'
@@ -120,9 +126,10 @@ public class ConnectorExecutor {
     }
 
     private void executeForOperator(List<Executable> body, int index) {
-        OperatorDTO operatorDTO = (OperatorDTO) body.get(index);
+        OperatorEx operatorDTO = (OperatorEx) body.get(index);
+        String leftValueReference = operatorDTO.getCondition().getLeft();
 
-        List<SchemaDTO> loopingList = executionManager.getValue(operatorDTO.getLeftValueReference()).getItems();
+        List<Object> loopingList = (List<Object>) executionManager.getValue(leftValueReference);
 
         if (ObjectUtils.isEmpty(loopingList)) {
             return;
