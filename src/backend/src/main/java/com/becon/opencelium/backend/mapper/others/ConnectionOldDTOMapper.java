4bf184a632a8087ce51222085a3d253a04340186
obidjon <obidsattarovich3600@gmail.com>
2024-02-02 15:53:59 +0500

    [Fixed] pushed ConnectionOldDTO #time 1m

---
+++ src/backend/src/main/java/com/becon/opencelium/backend/mapper/others/ConnectionOldDTOMapper.java
@@ -0,0 +1,25 @@
+package com.becon.opencelium.backend.mapper.others;
+
+import com.becon.opencelium.backend.mapper.base.Mapper;
+import com.becon.opencelium.backend.resource.connection.ConnectionDTO;
+import com.becon.opencelium.backend.resource.connection.ConnectionOldDTO;
+import org.mapstruct.Mapping;
+import org.mapstruct.Named;
+import org.mapstruct.ReportingPolicy;
+
+@org.mapstruct.Mapper(
+        componentModel = "spring",
+        unmappedTargetPolicy = ReportingPolicy.IGNORE,
+        unmappedSourcePolicy = ReportingPolicy.IGNORE,
+        uses = FieldBindingOldDTOMapper.class
+)
+@Named("connectionOldDTOMapper")
+public interface ConnectionOldDTOMapper extends Mapper<ConnectionDTO, ConnectionOldDTO> {
+    @Named("toDTO")
+    @Mapping(target = "fieldBindings", source = "fieldBinding",qualifiedByName = {"fieldBindingOldDTOMapper", "toDTOAll"})
+    ConnectionDTO toEntity(ConnectionOldDTO dto);
+
+    @Named("toOldDTO")
+    @Mapping(target = "fieldBinding", source = "fieldBindings",qualifiedByName = {"fieldBindingOldDTOMapper", "toOldDTOAll"})
+    ConnectionOldDTO toDTO(ConnectionDTO entity);
+}
