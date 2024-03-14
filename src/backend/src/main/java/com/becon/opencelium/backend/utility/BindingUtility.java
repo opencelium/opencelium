d468656b66b1af43e3fb711f73cd39803752a33b
obidjon <obidsattarovich3600@gmail.com>
2024-03-14 16:47:00 +0500

    [Modified] OC-715 #comment Implemented binding process for xml based methods #time 2h

--- src/backend/src/main/java/com/becon/opencelium/backend/utility/BindingUtility.java
+++ src/backend/src/main/java/com/becon/opencelium/backend/utility/BindingUtility.java
@@ -9,6 +9,109 @@ import java.util.*;
 
 public class BindingUtility {
 
+//---------------------------------------------- detach --------------------------------------------------//
+
+    public static void detach(List<MethodMng> methods, List<FieldBindingMng> fbs) {
+        for (MethodMng method : methods) {
+            if (method != null) {
+                if (method.getRequest() != null && method.getRequest().getBody() != null) {
+                    Map<String, Object> fields = method.getRequest().getBody().getFields();
+                    for (Map.Entry<String, Object> entry : fields.entrySet()) {
+                        entry.setValue(findRefAndReplace(entry.getValue(), fbs));
+                    }
+                }
+                if (method.getResponse() != null && method.getResponse() != null) {
+                    if (method.getResponse().getFail() != null && method.getResponse().getFail().getBody() != null) {
+                        Map<String, Object> fields = method.getResponse().getFail().getBody().getFields();
+                        for (Map.Entry<String, Object> entry : fields.entrySet()) {
+                            entry.setValue(findRefAndReplace(entry.getValue(), fbs));
+                        }
+                    }
+                    if (method.getResponse().getSuccess() != null && method.getResponse().getSuccess().getBody() != null) {
+                        Map<String, Object> fields = method.getResponse().getSuccess().getBody().getFields();
+                        for (Map.Entry<String, Object> entry : fields.entrySet()) {
+                            entry.setValue(findRefAndReplace(entry.getValue(), fbs));
+                        }
+                    }
+                }
+
+                for (FieldBindingMng fb : fbs) {
+                    if (method.getRequest().getEndpoint().contains("{%" + fb.getId() + "%}")) {
+                        method.getRequest().setEndpoint(method.getRequest().getEndpoint().replace("{%" + fb.getId() + "%}", getRefOfFBForPathOrHeader(fb.getFrom())));
+                    }
+                    if (method.getRequest().getHeader() != null) {
+                        for (Map.Entry<String, String> entry : method.getRequest().getHeader().entrySet()) {
+                            if (entry.getKey().equals("{%" + fb.getId() + "%}")) {
+                                entry.setValue(getRefOfFBForPathOrHeader(fb.getFrom()));
+                                break;
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    private static Object findRefAndReplace(Object obj, List<FieldBindingMng> fieldBinding) {
+        if (obj instanceof String str) {
+            if (str.matches(RegExpression.enhancement)) {
+                String id = str.replace("#{%", "")
+                        .replace("%}", "");
+
+                obj = getRefOfFB(id, fieldBinding);
+            }
+        } else if (obj instanceof List<?> list) {
+            List<Object> objects = new ArrayList<>();
+            for (Object o : list) {
+                objects.add(findRefAndReplace(o, fieldBinding));
+            }
+            obj = objects;
+        } else if (obj instanceof Map<?, ?> map) {
+            Map<String, Object> res = new LinkedHashMap<>();
+            for (Map.Entry<?, ?> entry : map.entrySet()) {
+                res.put((String) entry.getKey(), findRefAndReplace(entry.getValue(), fieldBinding));
+            }
+            obj = res;
+        }
+        return obj;
+    }
+
+    private static String getRefOfFB(String id, List<FieldBindingMng> fieldBinding) {
+        for (FieldBindingMng fb : fieldBinding) {
+            if (fb.getId().equals(id)) {
+                StringBuilder sb = new StringBuilder();
+                for (LinkedFieldMng from : fb.getFrom()) {
+                    sb.append(from.getColor())
+                            .append(".(")
+                            .append(from.getType())
+                            .append(")")
+                            .append(from.getField() == null ? "" : "." + from.getField())
+                            .append(";");
+                }
+                sb.deleteCharAt(sb.length() - 1);
+                return sb.toString();
+            }
+        }
+        throw new RuntimeException("ENHANCEMENT_NOT_FOUND");
+    }
+
+    private static String getRefOfFBForPathOrHeader(List<LinkedFieldMng> froms) {
+        StringBuilder sb = new StringBuilder();
+        for (LinkedFieldMng from : froms) {
+            sb.append(from.getColor())
+                    .append(".(")
+                    .append(from.getType())
+                    .append(")")
+                    .append(from.getField() == null ? "" : "." + from.getField())
+                    .append(";");
+        }
+        sb.deleteCharAt(sb.length() - 1);
+        return "{%" + sb + "%}";
+    }
+
+//--------------------------------------------------------------------------------------------------------//
+//---------------------------------------------- bind ----------------------------------------------------//
+
     public static String doWithPath(String endpoint, String id, List<LinkedFieldMng> from) {
         int indexOfQuestionSign = endpoint.indexOf("?");
         String query = null;
@@ -78,50 +181,8 @@ public class BindingUtility {
         };
     }
 
-    public static void detach(List<MethodMng> methods, List<FieldBindingMng> fbs) {
-        for (MethodMng method : methods) {
-            if (method != null) {
-                if (method.getRequest() != null && method.getRequest().getBody() != null) {
-                    Map<String, Object> fields = method.getRequest().getBody().getFields();
-                    for (Map.Entry<String, Object> entry : fields.entrySet()) {
-                        entry.setValue(findRefAndReplace(entry.getValue(), fbs));
-                    }
-                }
-                if (method.getResponse() != null && method.getResponse() != null) {
-                    if (method.getResponse().getFail() != null && method.getResponse().getFail().getBody() != null) {
-                        Map<String, Object> fields = method.getResponse().getFail().getBody().getFields();
-                        for (Map.Entry<String, Object> entry : fields.entrySet()) {
-                            entry.setValue(findRefAndReplace(entry.getValue(), fbs));
-                        }
-                    }
-                    if (method.getResponse().getSuccess() != null && method.getResponse().getSuccess().getBody() != null) {
-                        Map<String, Object> fields = method.getResponse().getSuccess().getBody().getFields();
-                        for (Map.Entry<String, Object> entry : fields.entrySet()) {
-                            entry.setValue(findRefAndReplace(entry.getValue(), fbs));
-                        }
-                    }
-                }
-
-                for (FieldBindingMng fb : fbs) {
-                    if (method.getRequest().getEndpoint().contains("{%" + fb.getId() + "%}")) {
-                        method.getRequest().setEndpoint(method.getRequest().getEndpoint().replace("{%" + fb.getId() + "%}", getRefOfFBForPathOrHeader(fb.getFrom())));
-                    }
-                    if(method.getRequest().getHeader()!=null){
-                        for (Map.Entry<String, String> entry : method.getRequest().getHeader().entrySet()) {
-                            if (entry.getKey().equals("{%" + fb.getId() + "%}")) {
-                                entry.setValue(getRefOfFBForPathOrHeader(fb.getFrom()));
-                                break;
-                            }
-                        }
-                    }
-                }
-            }
-        }
-    }
-
-    private static Map<String, Object> doWithXMLBody(Map<String, Object> src, List<String> fieldPaths, String id) {
-        return null;
-    }
+//--------------------------------------------------------------------------------------------------------//
+//---------------------------------------------JSON(bind)-------------------------------------------------//
 
     private static Map<String, Object> doWithJsonBody(Map<String, Object> src, List<String> fieldPaths, String id) {
         String name = fieldPaths.get(0);
@@ -129,11 +190,11 @@ public class BindingUtility {
         for (Map.Entry<String, Object> entry : src.entrySet()) {
             if (name.equals(entry.getKey())) { // object or primitive
                 fieldPaths.remove(0);
-                resultMap.put(entry.getKey(), process(entry.getValue(), fieldPaths, id, null));
+                resultMap.put(entry.getKey(), processJSON(entry.getValue(), fieldPaths, id, null));
             } else if (name.matches(entry.getKey() + "\\[.*]")) { //array
                 fieldPaths.remove(0);
                 String index = name.substring(name.indexOf('[') + 1, name.indexOf(']'));
-                resultMap.put(entry.getKey(), process(entry.getValue(), fieldPaths, id, index));
+                resultMap.put(entry.getKey(), processJSON(entry.getValue(), fieldPaths, id, index));
             } else {
                 resultMap.put(entry.getKey(), entry.getValue());
             }
@@ -142,7 +203,7 @@ public class BindingUtility {
     }
 
     @SuppressWarnings("unchecked")
-    private static Object process(Object value, List<String> fieldPaths, String id, String index) {
+    private static Object processJSON(Object value, List<String> fieldPaths, String id, String index) {
         if (index == null) {
             if (fieldPaths.isEmpty()) { // primitive type
                 return putIdToBody(value, id);
@@ -182,62 +243,86 @@ public class BindingUtility {
         }
     }
 
-    private static Object findRefAndReplace(Object obj, List<FieldBindingMng> fieldBinding) {
-        if (obj instanceof String str) {
-            if (str.matches(RegExpression.enhancement)) {
-                String id = str.replace("#{%", "")
-                        .replace("%}", "");
+//--------------------------------------------------------------------------------------------------------//
+//---------------------------------------------XML(bind)-------------------------------------------------//
 
-                obj = getRefOfFB(id, fieldBinding);
+    private static final String ocValue = "__oc__value";
+    private static final String ocAttributes = "__oc__attributes";
+
+    private static Map<String, Object> doWithXMLBody(Map<String, Object> src, List<String> fieldPaths, String id) {
+        String name = fieldPaths.get(0);
+        Map<String, Object> resultMap = new HashMap<>();
+        for (Map.Entry<String, Object> entry : src.entrySet()) {
+            if (name.equals(entry.getKey())) { // object or primitive
+                fieldPaths.remove(0);
+                resultMap.put(entry.getKey(), processXML(entry.getValue(), fieldPaths, id, null));
+            } else if (name.matches(entry.getKey() + "\\[.*]")) { //array
+                fieldPaths.remove(0);
+                String index = name.substring(name.indexOf('[') + 1, name.indexOf(']'));
+                resultMap.put(entry.getKey(), processXML(entry.getValue(), fieldPaths, id, index));
+            } else {
+                resultMap.put(entry.getKey(), entry.getValue());
             }
-        } else if (obj instanceof List<?> list) {
-            List<Object> objects = new ArrayList<>();
-            for (Object o : list) {
-                objects.add(findRefAndReplace(o, fieldBinding));
-            }
-            obj = objects;
-        } else if (obj instanceof Map<?, ?> map) {
-            Map<String, Object> res = new LinkedHashMap<>();
-            for (Map.Entry<?, ?> entry : map.entrySet()) {
-                res.put((String) entry.getKey(), findRefAndReplace(entry.getValue(), fieldBinding));
-            }
-            obj = res;
         }
-        return obj;
+        return resultMap;
     }
 
-    private static String getRefOfFB(String id, List<FieldBindingMng> fieldBinding) {
-        for (FieldBindingMng fb : fieldBinding) {
-            if (fb.getId().equals(id)) {
-                StringBuilder sb = new StringBuilder();
-                for (LinkedFieldMng from : fb.getFrom()) {
-                    sb.append(from.getColor())
-                            .append(".(")
-                            .append(from.getType())
-                            .append(")")
-                            .append(from.getField() == null ? "" : "." + from.getField())
-                            .append(";");
+    @SuppressWarnings("unchecked")
+    private static Object processXML(Object value, List<String> fieldPaths, String id, String index) {
+        if (index == null) {
+            if (fieldPaths.isEmpty() || fieldPaths.size() == 1 && fieldPaths.get(0).startsWith("@")) { // primitive type
+                if (value instanceof Map<?, ?>) {
+                    Map<String, Object> map = (Map<String, Object>) value;
+                    if (map.size() == 2 && map.containsKey(ocValue) && map.containsKey(ocAttributes)) {
+                        if (fieldPaths.isEmpty()) {
+                            map.put(ocValue, putIdToBody(map.get(ocValue), id));
+                        } else {
+                            map.put(ocAttributes, putIdToBody(map.get(ocAttributes), id));
+                        }
+                        return map;
+                    } else {
+                        throw new RuntimeException("UNABLE_TO_BIND : INCORRECT_PATH_OR_FIELD_NOT_FOUND");
+                    }
                 }
-                sb.deleteCharAt(sb.length() - 1);
-                return sb.toString();
+                return putIdToBody(value, id);
+            } else { // object
+                Map<String, Object> map = (Map<String, Object>) value;
+                return doWithXMLBody(map, fieldPaths, id);
+            }
+        } else { // array
+            if (index.isEmpty()) {
+                //ex. param[];
+                //if a field to be bound is whole array then we needn't process it. Just bind id.
+                return putIdToBody(value, id);
+            } else if (index.matches("[0-9]+")) {
+                //ex. param[1]
+                //it means that this field's type is array. We just need to look through 1st element of an array
+                int idx = Integer.parseInt(index);
+                List<Object> list = (List<Object>) value;
+                if (list.get(idx) instanceof Map<?, ?>) { //array of objects
+                    List<Map<String, Object>> mapList = (List<Map<String, Object>>) value;
+                    mapList.set(idx, doWithXMLBody(mapList.get(idx), fieldPaths, id));
+                    return mapList;
+                } else { // array of primitives
+                    list.set(idx, putIdToBody(list.get(idx), id));
+                    return list;
+                }
+            } else {
+                //ex. param[i]
+                //it means that this field's type is array. We need to look through every element of an array
+                List<Object> list = (List<Object>) value;
+                if (list.get(0) instanceof Map<?, ?>) { // array of objects
+                    List<Map<String, Object>> mapList = (List<Map<String, Object>>) value;
+                    return mapList.stream().map(f -> doWithXMLBody(f, fieldPaths, id));
+                } else { // array of primitives
+                    return Collections.singletonList(list.stream().map(f -> putIdToBody(f, id)).toList());
+                }
             }
         }
-        throw new RuntimeException("ENHANCEMENT_NOT_FOUND");
     }
 
-    private static String getRefOfFBForPathOrHeader(List<LinkedFieldMng> froms) {
-        StringBuilder sb = new StringBuilder();
-        for (LinkedFieldMng from : froms) {
-            sb.append(from.getColor())
-                    .append(".(")
-                    .append(from.getType())
-                    .append(")")
-                    .append(from.getField() == null ? "" : "." + from.getField())
-                    .append(";");
-        }
-        sb.deleteCharAt(sb.length() - 1);
-        return "{%" + sb + "%}";
-    }
+//--------------------------------------------------------------------------------------------------------//
+//-----------------------------------------------Others-----------------------------------------------------//
 
     private static String putIdToBody(Object value, String id) {
         //just returns 'wrapped' id. Might be changed!
