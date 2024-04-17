6357e0ad564f82b0231dfffc3ee34eabdce7c0a1
Bekhzod Kurbonboev <bekhzod_kurbonboev@epam.com>
2024-04-17 15:44:21 +0500

    [Modified] OC-798 #comment Changed contract in RequestEntityBuilder, solve ref: String -> Object (not SchemaDTO) #time 2h

--- src/backend/src/main/java/com/becon/opencelium/backend/execution/builder/BodyBuilder.java
+++ src/backend/src/main/java/com/becon/opencelium/backend/execution/builder/BodyBuilder.java
@@ -1,10 +1,9 @@
 package com.becon.opencelium.backend.execution.builder;
 
 import com.becon.opencelium.backend.resource.execution.OperationDTO;
-import com.becon.opencelium.backend.resource.execution.SchemaDTO;
 
 import java.util.function.Function;
 
 public interface BodyBuilder {
-    Object build(OperationDTO operation, Function<String, SchemaDTO> references);
+    Object build(OperationDTO operation, Function<String, Object> references);
 }
--- src/backend/src/main/java/com/becon/opencelium/backend/execution/builder/HeadersBuilder.java
+++ src/backend/src/main/java/com/becon/opencelium/backend/execution/builder/HeadersBuilder.java
@@ -1,11 +1,10 @@
 package com.becon.opencelium.backend.execution.builder;
 
 import com.becon.opencelium.backend.resource.execution.OperationDTO;
-import com.becon.opencelium.backend.resource.execution.SchemaDTO;
 import org.springframework.http.HttpHeaders;
 
 import java.util.function.Function;
 
 public interface HeadersBuilder {
-    HttpHeaders build(OperationDTO operation, Function<String, SchemaDTO> references);
+    HttpHeaders build(OperationDTO operation, Function<String, Object> references);
 }
