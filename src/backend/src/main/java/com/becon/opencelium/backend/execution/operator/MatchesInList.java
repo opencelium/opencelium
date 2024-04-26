1e87cf2e8c3dca67a9f2776a11a37a9c691d5e2e
Bob Mustaev <bobur.mustaev@becon.de>
2024-04-26 14:20:27 +0500

    [Fixed] OC-791 #comment Fixed allowList

--- src/backend/src/main/java/com/becon/opencelium/backend/execution/operator/MatchesInList.java
+++ src/backend/src/main/java/com/becon/opencelium/backend/execution/operator/MatchesInList.java
@@ -4,9 +4,11 @@ import java.util.Arrays;
 
 public class MatchesInList implements Operator {
 
+    // incoming values: o1 - String; o2 - %var1%, %var2, var3%
     @Override
     public <T, S> boolean apply(T o1, S o2) {
-        String[] values = (String[]) o2;
+        String[] values = o2.toString().replace("\n", ",").split(",");
+//        String[] values = (String[]) o2;
         Like like = new Like();
 
         return Arrays.stream(values).anyMatch(v -> like.apply(o1, v));
