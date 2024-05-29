1da944426d8a06c8c49ab400b64d5a06650358b2
Bekhzod Kurbonboev <bekhzod_kurbonboev@epam.com>
2024-05-29 12:07:34 +0500

    [Modified] OC-867 #comment For JSON and XML check against prestored MediaTypes #time 1h

--- src/backend/src/main/java/com/becon/opencelium/backend/utility/MediaTypeUtility.java
+++ src/backend/src/main/java/com/becon/opencelium/backend/utility/MediaTypeUtility.java
@@ -2,14 +2,42 @@ package com.becon.opencelium.backend.utility;
 
 import org.springframework.http.MediaType;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class MediaTypeUtility {
+    private static final Set<MediaType> JSON_COMPATIBLES;
+    private static final Set<MediaType> XML_COMPATIBLES;
+
+    static {
+        // initialize JSON compatible types
+        JSON_COMPATIBLES = new HashSet<>();
+        JSON_COMPATIBLES.add(null); // Json is default type
+        JSON_COMPATIBLES.add(MediaType.ALL); // Json is default type
+        JSON_COMPATIBLES.add(MediaType.APPLICATION_JSON);
+        JSON_COMPATIBLES.add(MediaType.APPLICATION_JSON_UTF8);
+        JSON_COMPATIBLES.add(MediaType.APPLICATION_GRAPHQL);
+        JSON_COMPATIBLES.add(MediaType.APPLICATION_NDJSON);
+        JSON_COMPATIBLES.add(MediaType.APPLICATION_PROBLEM_JSON);
+        JSON_COMPATIBLES.add(MediaType.APPLICATION_PROBLEM_JSON_UTF8);
+        JSON_COMPATIBLES.add(MediaType.APPLICATION_STREAM_JSON);
+
+        // initialize XML compatible types
+        XML_COMPATIBLES = new HashSet<>();
+        XML_COMPATIBLES.add(MediaType.APPLICATION_XML);
+        XML_COMPATIBLES.add(MediaType.APPLICATION_ATOM_XML);
+        XML_COMPATIBLES.add(MediaType.APPLICATION_PROBLEM_XML);
+        XML_COMPATIBLES.add(MediaType.APPLICATION_RSS_XML);
+        XML_COMPATIBLES.add(MediaType.APPLICATION_XHTML_XML);
+        XML_COMPATIBLES.add(MediaType.TEXT_XML);
+    }
 
     public static boolean isJsonCompatible(MediaType mediaType) {
-        return MediaType.APPLICATION_JSON.isCompatibleWith(mediaType);
+        return JSON_COMPATIBLES.contains(mediaType);
     }
 
     public static boolean isXmlCompatible(MediaType mediaType) {
-        return MediaType.APPLICATION_XML.isCompatibleWith(mediaType);
+        return XML_COMPATIBLES.contains(mediaType);
     }
 
     public static boolean isTextPlainCompatible(MediaType mediaType) {
