7d4307ae4a355056b0ca7905fc0150071cf6d57c
Bob Mustaev <bobur.mustaev@becon.de>
2024-06-03 16:18:43 +0200

    [Fixed] OC-719 #comment  fixed bug when migrating from old version to a new version.

---
+++ src/backend/src/main/java/com/becon/opencelium/backend/utility/ZipUtils.java
@@ -0,0 +1,59 @@
+package com.becon.opencelium.backend.utility;
+
+import org.apache.tomcat.util.http.fileupload.FileUtils;
+
+import java.io.*;
+import java.nio.file.*;
+import java.nio.file.attribute.BasicFileAttributes;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipInputStream;
+
+public class ZipUtils {
+
+    public static void extractZip(InputStream zipInputStream, Path rootPath) throws IOException {
+        File f = new File("../frontend");
+        FileUtils.deleteDirectory(f);
+        try (ZipInputStream zis = new ZipInputStream(new BufferedInputStream(zipInputStream))) {
+            ZipEntry zipEntry = zis.getNextEntry();
+
+            while (zipEntry != null) {
+                // Use only the file name, discarding any leading directories
+                String entryName = zipEntry.getName();
+                Path targetPath = rootPath.resolve(entryName).normalize();
+
+                // Ensure the entry does not escape the target directory
+                if (!targetPath.startsWith(rootPath)) {
+                    throw new IOException("Entry is outside of the target dir: " + zipEntry.getName());
+                }
+
+                if (zipEntry.isDirectory()) {
+                    Files.createDirectories(targetPath);
+                } else {
+                    Files.createDirectories(targetPath.getParent()); // Ensure parent directories exist
+                    Files.copy(zis, targetPath, StandardCopyOption.REPLACE_EXISTING);
+                }
+                zipEntry = zis.getNextEntry();
+            }
+        }
+    }
+
+    private static void clearFolder(Path folder) throws IOException {
+        if (Files.exists(folder)) {
+            Files.walkFileTree(folder, new SimpleFileVisitor<Path>() {
+                @Override
+                public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
+                    Files.delete(file);
+                    return FileVisitResult.CONTINUE;
+                }
+
+                @Override
+                public FileVisitResult postVisitDirectory(Path dir, IOException exc) throws IOException {
+                    if (!dir.equals(folder)) {
+                        Files.delete(dir);
+                    }
+                    return FileVisitResult.CONTINUE;
+                }
+            });
+        }
+    }
+}
