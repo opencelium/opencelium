2dbe1db22426f20d1d3f4abbd8aeeba97c423a90
Ruzalin Galiev <ruzalin.galiev@becon.de>
2024-06-10 13:16:24 +0200

    [Documentation] OC-143 updated conf.py #comment #time 15m

--- docs/conf.py
+++ docs/conf.py
@@ -1,9 +1,3 @@
-import sphinx
-from sphinx.util import logging
-
-logger = logging.getLogger(__name__)
-
-
 from __future__ import division, print_function, unicode_literals
 
 from datetime import datetime
@@ -35,35 +29,12 @@ html_js_files = [
 html_css_files = [
     'css/custom.css',
 ]
-
 def setup(app):
-    app.add_config_value('csp_header', '', 'html')
-    app.connect('builder-inited', add_csp_header)
-
-def add_csp_header(app):
-    if isinstance(app.builder, sphinx.builders.html.StandaloneHTMLBuilder):
-        header = app.config.csp_header
-        if header:
-            app.config.html_context['csp_header'] = header
-        else:
-            logger.warning("CSP header is not set. Please set 'csp_header' in conf.py.")
-
-# Add your desired CSP header here
-csp_policy = "default-src 'self'; frame-src 'self' docs.opencelium.io;"
-html_context = {
-    'csp_header': csp_policy,
-}
-
-def setup(app):
-    app.add_config_value('csp_header', '', 'html')
-    app.connect('builder-inited', add_csp_header)
-    app.add_js_file(None, body=f'''
-    <script>
-    document.addEventListener('DOMContentLoaded', function () {{
+    app.add_js_file(None, body="""
+    document.addEventListener('DOMContentLoaded', function () {
         let meta = document.createElement('meta');
         meta.httpEquiv = 'Content-Security-Policy';
-        meta.content = "{csp_policy}";
+        meta.content = "default-src 'self'; frame-src 'self' docs.opencelium.io;";
         document.getElementsByTagName('head')[0].appendChild(meta);
-    }});
-    </script>
-    ''')
+    });
+    """)
