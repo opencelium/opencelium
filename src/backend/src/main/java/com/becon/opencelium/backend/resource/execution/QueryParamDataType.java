0de6598bb0fd6ca2e068a7e00ca7a3f7a8e93f96
Bekhzod Kurbonboev <bekhzod_kurbonboev@epam.com>
2024-07-02 18:38:42 +0500

    [Modified] OC-889 #comment Create enum for query param typse and replace string literals #time 1h

---
+++ src/backend/src/main/java/com/becon/opencelium/backend/resource/execution/QueryParamDataType.java
@@ -0,0 +1,27 @@
+package com.becon.opencelium.backend.resource.execution;
+
+import org.apache.commons.lang3.StringUtils;
+
+public enum QueryParamDataType {
+    INT("int"),
+    STRING("string"),
+    DOUBLE("double"),
+    BOOLEAN("boolean"),
+    ARRAY("array");
+
+    private final String type;
+
+    QueryParamDataType(String type) {
+        this.type = type;
+    }
+
+    public static QueryParamDataType fromString(String type) {
+        for (QueryParamDataType dt : QueryParamDataType.values()) {
+            if (StringUtils.equalsIgnoreCase(type, dt.type)) {
+                return dt;
+            }
+        }
+
+        throw new IllegalArgumentException("No enum constant for type = '" + type + "' in query param data types");
+    }
+}
