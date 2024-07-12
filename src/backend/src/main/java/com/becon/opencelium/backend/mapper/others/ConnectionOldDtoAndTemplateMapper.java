43646e04e5ba2bc3483e5bec62c96f63b099d245
obidjon <obidsattarovich3600@gmail.com>
2024-07-12 19:15:27 +0500

    [Fixed] OC-847 #comment Changed templates style to old #time 1h

--- src/backend/src/main/java/com/becon/opencelium/backend/mapper/others/ConnectionOldDtoAndTemplateMapper.java
+++ src/backend/src/main/java/com/becon/opencelium/backend/mapper/others/ConnectionOldDtoAndTemplateMapper.java
@@ -1,7 +1,7 @@
 package com.becon.opencelium.backend.mapper.others;
 
 import com.becon.opencelium.backend.mapper.base.Mapper;
-import com.becon.opencelium.backend.resource.connection.ConnectionDTO;
+import com.becon.opencelium.backend.resource.connection.old.ConnectionOldDTO;
 import com.becon.opencelium.backend.resource.template.CtionTemplateResource;
 import org.mapstruct.Mapping;
 import org.mapstruct.Mappings;
@@ -12,21 +12,21 @@ import org.mapstruct.ReportingPolicy;
         unmappedTargetPolicy = ReportingPolicy.IGNORE,
         unmappedSourcePolicy = ReportingPolicy.IGNORE,
         uses = {
-                ConnectorDTOAndTemplateMapper.class
+                ConnectorOldDTOAndTemplateMapper.class
         }
 )
-public interface ConnectionDtoAndTemplateMapper
-        extends Mapper<ConnectionDTO, CtionTemplateResource> {
+public interface ConnectionOldDtoAndTemplateMapper
+        extends Mapper<ConnectionOldDTO, CtionTemplateResource> {
 
     @Mappings({
             @Mapping(target = "nodeId", ignore = true),
-            @Mapping(target = "fromConnector", qualifiedByName = {"ConnectorDTOAndTemplateMapper","toDTO"}),
-            @Mapping(target = "toConnector", qualifiedByName = {"ConnectorDTOAndTemplateMapper","toDTO"})
+            @Mapping(target = "fromConnector", qualifiedByName = {"ConnectorOldDTOAndTemplateMapper","toDTO"}),
+            @Mapping(target = "toConnector", qualifiedByName = {"ConnectorOldDTOAndTemplateMapper","toDTO"})
     })
-    CtionTemplateResource toDTO(ConnectionDTO entity);
+    CtionTemplateResource toDTO(ConnectionOldDTO entity);
 
     @Override
-    default ConnectionDTO toEntity(CtionTemplateResource dto){
+    default ConnectionOldDTO toEntity(CtionTemplateResource dto){
         return null;
     }
 }
