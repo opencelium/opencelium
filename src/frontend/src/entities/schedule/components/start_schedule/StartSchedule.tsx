0c070134658304104bd50288a61d1e147023a49a
Ruzalin Galiev <ruzalin.galiev@becon.de>
2024-11-06 10:51:31 +0100

    [Modified] OC-913 #comment fixed schedule list bug #time 10m

--- src/frontend/src/entities/schedule/components/start_schedule/StartSchedule.tsx
+++ src/frontend/src/entities/schedule/components/start_schedule/StartSchedule.tsx
@@ -10,7 +10,7 @@ import {getCurrentSubscription} from "@entity/license_management/redux_toolkit/a
 const StartSchedule = ({entity, scheduleModel, componentPermission}: any) => {
     const dispatch = useAppDispatch();
     const {currentSubscription, gettingCurrentSubscription} = Subscription.getReduxState();
-    const isDisabled = !currentSubscription || currentSubscription.currentOperationUsage >= currentSubscription.totalOperationUsage;
+    const isDisabled = !currentSubscription || (currentSubscription.currentOperationUsage >= currentSubscription.totalOperationUsage && currentSubscription.totalOperationUsage !== 0);
     useEffect(() => {
         dispatch(getCurrentSubscription())
     }, []);
