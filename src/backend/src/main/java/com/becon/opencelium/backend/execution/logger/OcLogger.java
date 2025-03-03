506bbf4d0ff2fa042e1aa3a14e8aea34589d2625
Bekhzod Kurbonboev <bekhzod_kurbonboev@epam.com>
2025-03-03 14:21:12 +0500

    [Modified] OC-1049 #comment When executing to collect support files write logs only in file do not propagate to parent logger #time 1h

--- src/backend/src/main/java/com/becon/opencelium/backend/execution/logger/OcLogger.java
+++ src/backend/src/main/java/com/becon/opencelium/backend/execution/logger/OcLogger.java
@@ -48,7 +48,7 @@ public class OcLogger<T extends LogMessage> {
 
             ch.qos.logback.classic.Logger logger = (ch.qos.logback.classic.Logger) LoggerFactory.getLogger("OcLogger-" + loggerId);
             logger.addAppender(fileAppender);
-            logger.setAdditive(true);
+            logger.setAdditive(false); // do not pass message to parent, just write to the file
 
             this.logger = logger;
         } else {
@@ -97,6 +97,11 @@ public class OcLogger<T extends LogMessage> {
 
 
     private <E> void logAndSend(Consumer<E> t, E message) {
+        if (createZip) {
+            t.accept(message);
+            return;
+        }
+
         if (!enable) {
             return;
         }
