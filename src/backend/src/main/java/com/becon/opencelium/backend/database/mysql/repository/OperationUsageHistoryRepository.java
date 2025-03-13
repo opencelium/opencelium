454142fd85b56041ab9c40238a956fbb65e67762
Bob Mustaev <bobur.mustaev@becon.de>
2025-03-13 10:25:34 +0100

    [Fixed] OC-992 #comment Added support for start and end date when fetching OperationUsage #time 2h

--- src/backend/src/main/java/com/becon/opencelium/backend/database/mysql/repository/OperationUsageHistoryRepository.java
+++ src/backend/src/main/java/com/becon/opencelium/backend/database/mysql/repository/OperationUsageHistoryRepository.java
@@ -4,8 +4,10 @@ import com.becon.opencelium.backend.database.mysql.entity.OperationUsageHistory;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDateTime;
 import java.util.Optional;
 
 @Repository
@@ -13,4 +15,13 @@ public interface OperationUsageHistoryRepository extends JpaRepository<Operation
 
     Optional<OperationUsageHistory> findByConnectionTitle(String title);
     Page<OperationUsageHistory> findAll(Pageable pageable);
+
+    @Query("SELECT DISTINCT h FROM OperationUsageHistory h JOIN h.details d " +
+            "WHERE (:startDate IS NULL OR d.startDate >= :startDate) " +
+            "AND (:endDate IS NULL OR d.startDate <= :endDate)")
+    Page<OperationUsageHistory> findAllByDetailsStartDateBetween(
+            LocalDateTime start,
+            LocalDateTime end,
+            Pageable pageable
+    );
 }
