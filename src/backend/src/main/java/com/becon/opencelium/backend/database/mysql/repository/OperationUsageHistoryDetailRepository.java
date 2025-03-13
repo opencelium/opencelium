7deee6481f5cc628a0340f75f6e3421e2a50a604
Bob Mustaev <bobur.mustaev@becon.de>
2025-03-13 12:18:49 +0100

    [Fixed] OC-992 #comment fixed bug that was prevented proper filtering by date #time 2h

--- src/backend/src/main/java/com/becon/opencelium/backend/database/mysql/repository/OperationUsageHistoryDetailRepository.java
+++ src/backend/src/main/java/com/becon/opencelium/backend/database/mysql/repository/OperationUsageHistoryDetailRepository.java
@@ -4,7 +4,22 @@ import com.becon.opencelium.backend.database.mysql.entity.OperationUsageHistoryD
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.time.LocalDateTime;
+import java.util.List;
 
 public interface OperationUsageHistoryDetailRepository extends JpaRepository<OperationUsageHistoryDetail, Long> {
-    Page<OperationUsageHistoryDetail> findAllByOperationUsageHistoryId(String usageId, Pageable pageable);
+    Page<OperationUsageHistoryDetail> findAllByOperationUsageHistoryId(Long usageId, Pageable pageable);
+
+    @Query("SELECT d FROM OperationUsageHistoryDetail d " +
+            "WHERE d.operationUsageHistory.id = :historyId " +
+            "AND (:startDate IS NULL OR d.startDate >= :startDate) " +
+            "AND (:endDate IS NULL OR d.startDate <= :endDate)")
+    Page<OperationUsageHistoryDetail> findDetailsByHistoryIdAndStartDateBetween(
+            @Param("historyId") Long historyId,
+            @Param("startDate") LocalDateTime startDate,
+            @Param("endDate") LocalDateTime endDate,
+            Pageable pageable);
 }
--- src/backend/src/main/java/com/becon/opencelium/backend/database/mysql/service/OperationUsageHistoryDetailService.java
+++ src/backend/src/main/java/com/becon/opencelium/backend/database/mysql/service/OperationUsageHistoryDetailService.java
@@ -2,9 +2,16 @@ package com.becon.opencelium.backend.database.mysql.service;
 
 import com.becon.opencelium.backend.database.mysql.entity.OperationUsageHistoryDetail;
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
+import java.time.LocalDateTime;
+import java.util.List;
 
 public interface OperationUsageHistoryDetailService {
     void save(OperationUsageHistoryDetail detail);
-    Page<OperationUsageHistoryDetail> getAllUsageDetailsByOperationUsageHistoryId(String usageId,int page, int size,
+    Page<OperationUsageHistoryDetail> getAllUsageDetailsByOperationUsageHistoryId(Long usageId,int page, int size,
                                                                                   String[] sort);
+
+    Page<OperationUsageHistoryDetail> findDetailsByHistoryIdAndStartDateBetween(Long historyId, LocalDateTime startDate, LocalDateTime endDate, Pageable pageable);
+
 }
