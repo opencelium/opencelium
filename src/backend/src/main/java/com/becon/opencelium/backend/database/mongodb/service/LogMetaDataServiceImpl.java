972005b780e02e5e05e7a7521b2fc10f2c4a0958
obidjon <obidsattarovich3600@gmail.com>
2025-04-27 22:22:10 +0500

    [Added] OC-1090 #comment Implemented saving Metadata to Mongodb #time 30m

--- src/backend/src/main/java/com/becon/opencelium/backend/database/mongodb/service/LogMetaDataServiceImpl.java
+++ src/backend/src/main/java/com/becon/opencelium/backend/database/mongodb/service/LogMetaDataServiceImpl.java
@@ -1,12 +1,20 @@
 package com.becon.opencelium.backend.database.mongodb.service;
 
 import com.becon.opencelium.backend.database.mongodb.entity.LogMetaData;
+import com.becon.opencelium.backend.database.mongodb.repository.LogMetaDataRepository;
 import org.springframework.stereotype.Service;
 
 @Service
 public class LogMetaDataServiceImpl implements LogMetaDataService {
+
+    private final LogMetaDataRepository repository;
+
+    public LogMetaDataServiceImpl(LogMetaDataRepository repository) {
+        this.repository = repository;
+    }
+
     @Override
     public void save(LogMetaData meta) {
-        // TODO: OC-1090
+        repository.save(meta);
     }
 }
