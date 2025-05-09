2bc539e3af5b6f17ba67413bc738e252a24be334
Bekhzod Kurbonboev <bekhzodkurbonboev@gmail.com>
2025-05-09 13:05:09 +0500

    [Modified] OC-1067 #comment Set avarage execution time for RunningJobsResource #time 20m

--- src/backend/src/main/java/com/becon/opencelium/backend/database/mysql/service/SchedulerServiceImp.java
+++ src/backend/src/main/java/com/becon/opencelium/backend/database/mysql/service/SchedulerServiceImp.java
@@ -60,6 +60,7 @@ public class SchedulerServiceImp implements SchedulerService {
     private final SchedulingStrategy schedulingStrategy;
     private final SchedulerRepository schedulerRepository;
     private final NotificationRepository notificationRepository;
+    private final ExecutionServiceImp executionServiceImp;
     private final Mapper<Connection, ConnectionDTO> connectionMapper;
 
 
@@ -73,6 +74,7 @@ public class SchedulerServiceImp implements SchedulerService {
             SchedulerRepository schedulerRepository,
             NotificationRepository notificationRepository,
             SchedulerFactoryBean schedulerFactoryBean,
+            ExecutionServiceImp executionServiceImp,
             Mapper<Connection, ConnectionDTO> connectionMapper
     ) {
         this.connectionService = connectionService;
@@ -83,6 +85,7 @@ public class SchedulerServiceImp implements SchedulerService {
         this.schedulingStrategy = SchedulerFactory.createQuartzScheduler(schedulerFactoryBean.getScheduler());
         this.notificationRepository = notificationRepository;
         this.schedulerRepository = schedulerRepository;
+        this.executionServiceImp = executionServiceImp;
         this.connectionMapper = connectionMapper;
         this.connectorService = connectorService;
     }
@@ -270,12 +273,17 @@ public class SchedulerServiceImp implements SchedulerService {
             Scheduler scheduler = getById(schedId);
             jobsResource.setSchedulerId(scheduler.getId());
             jobsResource.setTitle(scheduler.getTitle());
-            Connection connection = connectionService.getById(connId);
 
+            // TODO: need to rework calculation of avg time
+            double avg = executionServiceImp.getAvgDurationOfExecution(schedId);
+            jobsResource.setAvgDuration(avg);
+
+            Connection connection = connectionService.getById(connId);
             Connector fromCotr = connectorService.getById(connection.getFromConnector());
             Connector toCtor = connectorService.getById(connection.getToConnector());
             jobsResource.setToConnector(toCtor.getTitle());
             jobsResource.setFromConnector(fromCotr.getTitle());
+
             runningJobsResources.add(jobsResource);
         });
         return runningJobsResources;
@@ -291,12 +299,17 @@ public class SchedulerServiceImp implements SchedulerService {
                 Scheduler scheduler = getById(schedId);
                 jobsResource.setSchedulerId(scheduler.getId());
                 jobsResource.setTitle(scheduler.getTitle());
-                Connection connection = connectionService.getById(connId);
 
+                // TODO: need to rework calculation of avg time
+                double avg = executionServiceImp.getAvgDurationOfExecution(schedId);
+                jobsResource.setAvgDuration(avg);
+
+                Connection connection = connectionService.getById(connId);
                 Connector fromCotr = connectorService.getById(connection.getFromConnector());
                 Connector toCtor = connectorService.getById(connection.getToConnector());
                 jobsResource.setToConnector(toCtor.getTitle());
                 jobsResource.setFromConnector(fromCotr.getTitle());
+
                 runningJobsResources.add(jobsResource);
             }
         });
