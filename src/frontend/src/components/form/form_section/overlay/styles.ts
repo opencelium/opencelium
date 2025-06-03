4d1c3adcc9f004ee89b027cb8ba49f693a0747c8
Ruzalin Galiev <ruzalin.galiev@becon.de>
2025-06-03 15:54:45 +0200

    [Modified] OC-1112 #comment updated styles #time 30m

--- src/frontend/src/components/form/form_section/overlay/styles.ts
+++ src/frontend/src/components/form/form_section/overlay/styles.ts
@@ -6,13 +6,13 @@ export const OverlayContainer = styled.div`
     width: 100%;
     height: 100%;
     background: white;
-    opacity: 0.7;
+    opacity: 0.9;
 `;
-export const ComponentContainer = styled.div`
+export const ComponentContainer = styled.div`    
+    width: 100%;
+    height: 100%;
     display: flex;
     justify-content: center;
-    align-items: center;
     position: absolute;
-    left: 0;
-    width: 100%;
+    top: 0;
 `;
