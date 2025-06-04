4e255c4cdd2b92bd2323b4400e4a23d6a159727a
Bob Mustaev <bobur.mustaev@becon.de>
2025-06-04 15:56:11 +0500

    [Modified] OC-1067 #comment modified HeaderConstants to X-Master-Password #time 10m

--- src/backend/src/main/java/com/becon/opencelium/backend/constant/HeaderConstants.java
+++ src/backend/src/main/java/com/becon/opencelium/backend/constant/HeaderConstants.java
@@ -1,5 +1,5 @@
 package com.becon.opencelium.backend.constant;
 
 public interface HeaderConstants {
-    String MASTER_PASSWORD = "master_password";
+    String MASTER_PASSWORD = "X-Master-Password";
 }
