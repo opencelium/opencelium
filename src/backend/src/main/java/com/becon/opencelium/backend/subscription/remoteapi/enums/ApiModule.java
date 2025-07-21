4c1b101c1354b4f96f85c7bdd19c8e979ff6876d
Bekhzod Kurbonboev <bekhzodkurbonboev@gmail.com>
2025-07-22 03:14:21 +0500

    [Added] OC-959 #comment Created module for Invoker, amplemented necessary methods, adjusted factory enum #time 2h

--- src/backend/src/main/java/com/becon/opencelium/backend/subscription/remoteapi/enums/ApiModule.java
+++ src/backend/src/main/java/com/becon/opencelium/backend/subscription/remoteapi/enums/ApiModule.java
@@ -1,10 +1,13 @@
 package com.becon.opencelium.backend.subscription.remoteapi.enums;
 
-import com.becon.opencelium.backend.subscription.remoteapi.module.*;
+import com.becon.opencelium.backend.subscription.remoteapi.module.InvokerModule;
+import com.becon.opencelium.backend.subscription.remoteapi.module.ReportModule;
+import com.becon.opencelium.backend.subscription.remoteapi.module.SubscriptionModule;
 
 public enum ApiModule {
     SUBSCRIPTION(SubscriptionModule.class),
-    OPERATION_USAGE(ReportModule.class);
+    OPERATION_USAGE(ReportModule.class),
+    INVOKER(InvokerModule.class);
 
     private final Class<?> moduleClass;
 
