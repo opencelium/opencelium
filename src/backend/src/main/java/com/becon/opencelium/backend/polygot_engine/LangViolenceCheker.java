88ba96534fa35a5f0ae2ce5b0887e7cbd27e09ed
obidjon <obidsattarovich3600@gmail.com>
2025-07-30 10:21:39 +0500

    [Modified] #comment OC-1167 Removed violence checking strategy #time 10m

--- src/backend/src/main/java/com/becon/opencelium/backend/polygot_engine/LangViolenceCheker.java
+++
@@ -1,7 +0,0 @@
-package com.becon.opencelium.backend.polygot_engine;
-
-import com.becon.opencelium.backend.polygot_engine.ex.InvalidScriptException;
-
-public interface LangViolenceCheker {
-    void check(String script) throws InvalidScriptException;
-}
