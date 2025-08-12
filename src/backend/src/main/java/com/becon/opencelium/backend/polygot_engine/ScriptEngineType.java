8b56a93b53e18de180a369e91a1fd393dbaf0d4d
obidjon <obidsattarovich3600@gmail.com>
2025-08-13 00:18:30 +0500

    [Added] #comment Implemented Ruby support with GraalVM #time 4h

--- src/backend/src/main/java/com/becon/opencelium/backend/polygot_engine/ScriptEngineType.java
+++ src/backend/src/main/java/com/becon/opencelium/backend/polygot_engine/ScriptEngineType.java
@@ -27,7 +27,7 @@ public enum ScriptEngineType {
         return switch (this) {
             case NASHORN -> List.of(LanguageType.JS);
             case JYTHON -> List.of(LanguageType.PYTHON_2);
-            case GRAALVM -> List.of(LanguageType.JS, LanguageType.PYTHON_3);
+            case GRAALVM -> List.of(LanguageType.JS, LanguageType.PYTHON_3, LanguageType.RUBY);
         };
     }
 }
