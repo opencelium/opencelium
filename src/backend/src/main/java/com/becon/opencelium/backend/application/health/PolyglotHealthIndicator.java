79c145a8e61d7bda38db6980586c98e009ba7cd2
Bob Mustaev <bobur.mustaev@becon.de>
2025-10-21 11:18:02 +0200

    [Fixed] OC-1232 Fixed health indicator. now return always 200 ok #time 10m

--- src/backend/src/main/java/com/becon/opencelium/backend/application/health/PolyglotHealthIndicator.java
+++ src/backend/src/main/java/com/becon/opencelium/backend/application/health/PolyglotHealthIndicator.java
@@ -7,30 +7,30 @@ import org.springframework.boot.actuate.health.AbstractHealthIndicator;
 import org.springframework.boot.actuate.health.Health;
 import org.springframework.security.core.parameters.P;
 import org.springframework.stereotype.Component;
-//
-//@Component("polyglot")
-//public class PolyglotHealthIndicator extends AbstractHealthIndicator {
-//
-//    private final PolyglotServiceGRPCClient polyglotServiceGRPCClient;
-//
-//    public PolyglotHealthIndicator(PolyglotServiceGRPCClient polyglotServiceGRPCClient) {
-//        this.polyglotServiceGRPCClient = polyglotServiceGRPCClient;
-//    }
-//
-//    @Override
-//    protected void doHealthCheck(Health.Builder builder) {
-//        builder.withDetail("name", "Polyglot");
-//
-//        try {
-//            if (polyglotServiceGRPCClient.isUp()) {
-//                builder.up();
-//            } else {
-//                builder.down()
-//                        .withDetail("error", "Couldn't establish a connection to Polyglot service. Check if it is running.");
-//            }
-//        } catch (Exception e) {
-//            builder.down()
-//                    .withDetail("error", e.getMessage() != null ? e.getMessage() : e.getClass().getSimpleName());
-//        }
-//    }
-//}
+
+@Component("polyglot")
+public class PolyglotHealthIndicator extends AbstractHealthIndicator {
+
+    private final PolyglotServiceGRPCClient polyglotServiceGRPCClient;
+
+    public PolyglotHealthIndicator(PolyglotServiceGRPCClient polyglotServiceGRPCClient) {
+        this.polyglotServiceGRPCClient = polyglotServiceGRPCClient;
+    }
+
+    @Override
+    protected void doHealthCheck(Health.Builder builder) {
+        builder.withDetail("name", "Polyglot");
+
+        try {
+            if (polyglotServiceGRPCClient.isUp()) {
+                builder.up();
+            } else {
+                builder.down()
+                        .withDetail("error", "Couldn't establish a connection to Polyglot service. Check if it is running.");
+            }
+        } catch (Exception e) {
+            builder.down()
+                    .withDetail("error", e.getMessage() != null ? e.getMessage() : e.getClass().getSimpleName());
+        }
+    }
+}
