6b1df3e90d2694f9bd5ee7c479f9503bce6b2da2
Bob Mustaev <bobur.mustaev@becon.de>
2025-12-04 17:05:53 +0500

    [Fixed] OC-1289 fixed bug where invoker file didn't remove from cashed memory #time 2h

--- src/backend/src/main/java/com/becon/opencelium/backend/invoker/service/InvokerServiceImp.java
+++ src/backend/src/main/java/com/becon/opencelium/backend/invoker/service/InvokerServiceImp.java
@@ -140,8 +140,10 @@ public class InvokerServiceImp implements InvokerService {
     public void deleteInvokerFile(String name) {
         try {
             Path file = findFileByInvokerName(name).toPath();
-            if (exists(file)) {
+            if (invokerContainer.existsByName(name)) {
                 invokerContainer.remove(name);
+            }
+            if (exists(file)) {
                 Files.delete(file.toAbsolutePath());
 
                 // delete invoker sync record
@@ -202,10 +204,12 @@ public class InvokerServiceImp implements InvokerService {
                 return;
             }
             InvokerParserImp parser = new InvokerParserImp(document);
-            File f = new File(document.getDocumentURI());
-            String invoker = FileNameUtils.removeExtension(f.getName());
-            invoker = invoker.replace("%20", " ");
-            container.put(invoker, parser.parse());
+//            File f = new File(document.getDocumentURI());
+//            String invoker = FileNameUtils.removeExtension(f.getName());
+//            invoker = invoker.replace("%20", " ");
+            Invoker invoker =  parser.parse();
+            String invokerName = invoker.getName();
+            container.put(invokerName, invoker);
         });
         return container;
     }
