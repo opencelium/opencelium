d6bde4b8d417e65a167bbbe1fedd9fdc8cae2f31
Ruzalin Galiev <ruzalin.galiev@becon.de>
2025-12-11 11:18:23 +0100

    [Modified] OC-1159 #comment converted ai result #time 1h

--- src/frontend/src/components/connection_logs/ConnectorPanel/TraceItem/MethodTrace/AIResultsContainer.tsx
+++ src/frontend/src/components/connection_logs/ConnectorPanel/TraceItem/MethodTrace/AIResultsContainer.tsx
@@ -1,12 +1,23 @@
 import React from 'react';
 
 const AiResultsContainer = ({aiResults}: {aiResults: any}) => {
+    const results: {chromaDBResult: any[], aiBotResult: string, hasAI: boolean} = aiResults || {chromaDBResult: [], aiBotResult: '', hasAI: false};
+    const hasAiBot = results.hasAI;
+    const aiBotResults: string[] = hasAiBot ? results.aiBotResult.split('### ') : [];
+    const aibotName = hasAiBot ? aiBotResults.shift() : 'no bot';
     return (
         <div style={{marginLeft: '40px', marginTop: '10px'}}>
             <p><strong>AI possible solutions/descriptions: </strong></p>
-            {aiResults.map((result: any, index: number) => (
-                <p>{`${index + 1}. ${result?.metadata?.solution || result?.metadata?.description}`}</p>
-            ))}
+            {hasAiBot ? <span>{`${aibotName}`}</span> : <span>AI bot is not set</span>}
+            {hasAiBot ?
+                aiBotResults.map((result: any, index: number) => (
+                    <p>{`${result}`}</p>
+                ))
+                :
+                results.chromaDBResult.map((result: any, index: number) => (
+                    <p>{`${index + 1}. ${result?.metadata?.solution || result?.metadata?.description}`}</p>
+                ))
+            }
         </div>
     )
 }
