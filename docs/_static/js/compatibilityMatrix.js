75e147c71e6a35bef9c9ab90b2f12128e3cca385
jaysemere <jakob.semere@becon.de>
2026-05-08 17:28:05 +0200

    Update compatibilityMatrix.js

--- docs/_static/js/compatibilityMatrix.js
+++ docs/_static/js/compatibilityMatrix.js
@@ -25,7 +25,7 @@ async function loadCompatibility() {
             },
             {
                 data: 'status',
-                title: '',
+                title: null,
                 render: function (data) {
                     if (data === "OK") return "✅";
                     if (data === "PARTIAL") return "⚠️";
@@ -66,7 +66,7 @@ async function loadCompatibility() {
         const column = this;
         const title = $(column.header()).text();
 
-        if (title ==! "" || title === 'Notes') return;
+        if (title === null || title === 'Notes') return;
 
         // unique values holen
         const uniqueValues = new Set();
