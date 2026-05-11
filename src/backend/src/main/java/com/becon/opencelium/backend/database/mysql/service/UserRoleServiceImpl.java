55cb033a83e039ce091635abbb115b9dd5a5b970
obidjon <obidsattarovich3600@gmail.com>
2026-05-11 19:11:24 +0500

    [Fixed] #comment Fixed incorrect bean names on UserRoleServiceImpl #time 10m

--- src/backend/src/main/java/com/becon/opencelium/backend/database/mysql/service/UserRoleServiceImpl.java
+++ src/backend/src/main/java/com/becon/opencelium/backend/database/mysql/service/UserRoleServiceImpl.java
@@ -20,7 +20,6 @@ import com.becon.opencelium.backend.database.mysql.entity.RoleHasPermission;
 import com.becon.opencelium.backend.database.mysql.entity.UserRole;
 import com.becon.opencelium.backend.database.mysql.repository.UserRoleRepository;
 import com.becon.opencelium.backend.resource.user.UserRoleResource;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
 
@@ -32,12 +31,12 @@ import java.util.stream.Collectors;
 @Service
 public class UserRoleServiceImpl implements UserRoleService {
 
-    private UserRoleRepository userRoleRepository;
-    private PermissionService permissionService;
-    private ComponentService componentService;
+    private final UserRoleRepository userRoleRepository;
+    private final PermissionService permissionService;
+    private final ComponentService componentService;
 
-    public UserRoleServiceImpl(@Qualifier("PermissionServiceImpl") PermissionService permissionService,
-                               @Qualifier("ComponentServiceImpl") ComponentService componentService,
+    public UserRoleServiceImpl(@Qualifier("permissionServiceImpl") PermissionService permissionService,
+                               @Qualifier("componentServiceImpl") ComponentService componentService,
                                UserRoleRepository userRoleRepository) {
         this.userRoleRepository = userRoleRepository;
         this.permissionService = permissionService;
