e4445de0e31f70448448fbefb7298f34489f5359
behzodkurbonboev <bekhzodkurbonboev@gmail.com>
2026-06-22 11:57:00 +0500

    [Fixed] OC-1447: Remove check for 'COMPLETED' when searching children… (#228)
    * [Fixed] OC-1447: Remove check for 'COMPLETED' when searching children from buffer
    
    * [Fixed] OC-1447: Remove 'buffer' word in method naming

--- src/backend/src/main/java/com/becon/opencelium/backend/execution/logger/service/LogDataServiceImp.java
+++ src/backend/src/main/java/com/becon/opencelium/backend/execution/logger/service/LogDataServiceImp.java
@@ -345,7 +345,7 @@ public class LogDataServiceImp implements LogDataService {
         return metaDataLogRepository.findById(phaseId)
                 .or(() -> metaDataLogRepository.findByExecutionIdAndType(phaseId, EXECUTION.name()))
                 .or(() -> metaDataLogRepository.findByFlowIdAndType(phaseId, FLOWCHART.name()))
-                .or(() -> buffer.findInBufferById(phaseId))
+                .or(() -> buffer.findById(phaseId))
                 .orElseThrow(() -> new RuntimeException("LogData element not found with specified id = " + phaseId));
     }
 
@@ -369,7 +369,7 @@ public class LogDataServiceImp implements LogDataService {
 
         var savedChildren = metaDataLogRepository.findChildren(executionId, FLOWCHART.name(), SORT_ASCENDING);
 
-        var bufferedChildren = buffer.findAllCompletedByExecutionId(executionId).stream()
+        var bufferedChildren = buffer.findAllByExecutionId(executionId).stream()
                 .filter(child -> child.getType() == FLOWCHART)
                 .toList();
 
@@ -425,7 +425,7 @@ public class LogDataServiceImp implements LogDataService {
     private List<LogDataMng> findBufferedChildren(String executionId, String flowchartId, String regex, String loopIndex) {
         Pattern pattern = Pattern.compile(regex);
 
-        return buffer.findAllCompletedByExecutionId(executionId).stream()
+        return buffer.findAllByExecutionId(executionId).stream()
                 .filter(child -> Objects.equals(child.getFlowId(), flowchartId))
                 .filter(child -> child.getIndexPath() != null)
                 .filter(child -> pattern.matcher(child.getIndexPath()).matches())
@@ -501,7 +501,7 @@ public class LogDataServiceImp implements LogDataService {
      * First check in-memory buffer, then DB.
      */
     private Optional<LogDataMng> findExistingBlock(LogDataMng block) {
-        Optional<LogDataMng> inBuffer = buffer.findInBufferByKey(block);
+        Optional<LogDataMng> inBuffer = buffer.findByKey(block);
         if (inBuffer.isPresent()) {
             return inBuffer;
         }
