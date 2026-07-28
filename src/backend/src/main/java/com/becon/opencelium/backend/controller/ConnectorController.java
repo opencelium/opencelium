3cef5626b1be32ba6b0a5895271bbdbafbadfedd
Bekhzod Kurbonboev <bekhzodkurbonboev@gmail.com>
2026-07-28 18:11:40 +0500

    [OC-1514] Remove manual flag reversal for sslCert

--- src/backend/src/main/java/com/becon/opencelium/backend/controller/ConnectorController.java
+++ src/backend/src/main/java/com/becon/opencelium/backend/controller/ConnectorController.java
@@ -104,11 +104,7 @@ public class ConnectorController {
     @GetMapping
     public ResponseEntity<?> getByTitle(@RequestParam String title) {
         return connectorService.findAllByTitle(title)
-                .map(c -> {
-                    ConnectorResource resource = connectorResourceMapper.toDTO(c);
-                    resource.setSslCert(resource.isSslCert());
-                    return ResponseEntity.ok().body(resource);
-                })
+                .map(c -> ResponseEntity.ok().body(connectorResourceMapper.toDTO(c)))
                 .orElseThrow(() -> new ConnectorNotFoundException(title));
     }
 
@@ -137,11 +133,7 @@ public class ConnectorController {
     @GetMapping(path = "/{id}")
     public ResponseEntity<?> get(@PathVariable int id) {
         return connectorService.findById(id)
-                .map(c -> {
-                    ConnectorResource resource = connectorResourceMapper.toDTO(c);
-                    resource.setSslCert(resource.isSslCert());
-                    return ResponseEntity.ok().body(resource);
-                })
+                .map(c -> ResponseEntity.ok().body(connectorResourceMapper.toDTO(c)))
                 .orElseThrow(() -> new ConnectorNotFoundException(id));
     }
 
@@ -169,10 +161,6 @@ public class ConnectorController {
     @GetMapping("/all")
     public ResponseEntity<List<ConnectorResource>> getAll() {
         List<ConnectorResource> connectorResources = connectorResourceMapper.toDTOAll(connectorService.findAll());
-        connectorResources.stream().forEach(ctor -> {
-            boolean validationSslDisabled = !ctor.isSslCert();
-            ctor.setSslCert(validationSslDisabled);
-        });
         return ResponseEntity.ok(connectorResources);
     }
 
@@ -195,8 +183,6 @@ public class ConnectorController {
         connectorResources.forEach(x -> {
             x.getInvoker().setOperations(null);
             x.getInvoker().setRequiredData(null);
-            boolean validationSslDisabled = !x.isSslCert();
-            x.setSslCert(validationSslDisabled);
         });
 
         return ResponseEntity.ok(connectorResources);
