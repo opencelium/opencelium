14d3be38eb18ee4f372ee83fb0805b24dae68a87
Bekhzod Kurbonboev <bekhzodkurbonboev@gmail.com>
2026-08-12 08:08:59 +0500

    [OC-1541] Add unit tests for email validation

--- src/backend/src/main/java/com/becon/opencelium/backend/utility/EmailUtility.java
+++ src/backend/src/main/java/com/becon/opencelium/backend/utility/EmailUtility.java
@@ -2,14 +2,17 @@ package com.becon.opencelium.backend.utility;
 
 import java.util.regex.Pattern;
 
-public class EmailUtility {
-    private static final Pattern pattern = Pattern.compile("^[a-zA-Z0-9_+&*-]+(?:\\.[a-zA-Z0-9_+&*-]+)*@(?:[a-zA-Z0-9-]+\\.)+[a-zA-Z]{2,7}$");
+public final class EmailUtility {
+    private static final Pattern EMAIL_PATTERN = Pattern.compile("^[a-zA-Z0-9_+&*-]+(?:\\.[a-zA-Z0-9_+&*-]+)*@(?:[a-zA-Z0-9-]+\\.)+[a-zA-Z]{2,7}$");
+
+    private EmailUtility() {
+    }
 
     public static boolean isValid(String email) {
         if (email == null) {
             return false;
         }
 
-        return pattern.matcher(email).matches();
+        return EMAIL_PATTERN.matcher(email).matches();
     }
 }
