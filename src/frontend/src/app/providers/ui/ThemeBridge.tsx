2705e19cd17e7020adbd828cddfcd326afa13608
Ramil Davidov <ramil.davydov@yahoo.com>
2026-08-18 17:26:51 +0500

    [Fixed] OC-1549 #comment Fix barely-visible read-only fields in dark theme #time 2h

--- src/frontend/src/app/providers/ui/ThemeBridge.tsx
+++ src/frontend/src/app/providers/ui/ThemeBridge.tsx
@@ -105,7 +105,7 @@ export const ThemeBridge = ({ children }) => {
 
                     colorText: theme.color.text.primary,
                     colorTextSecondary: theme.color.text.secondary,
-                    colorTextDisabled: theme.color.text.disabled,
+                    colorTextDisabled: theme.color.text.secondary,
                     colorTextPlaceholder: theme.color.text.disabled,
 
                     colorBorder: theme.color.border.default,
